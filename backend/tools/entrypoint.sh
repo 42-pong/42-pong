abaeadc07d921e03f5da7175178486e83603f5f9
sawamura-hayato <sunzecun@gmail.com>
2024-11-12 11:59:27 +0900

    :constrution: entrypoint.shにsuperuser作成するコマンド追加
    createsuperuserのコマンドを使用しなかった理由
      createsuperuserだとパスワードはシェルで登録する必要があるため

--- backend/tools/entrypoint.sh
+++ backend/tools/entrypoint.sh
@@ -2,5 +2,16 @@
 
 python3 manage.py migrate --noinput
 
+# todo: username, email. passwordの環境変数で登録
+python3 manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+username = 'admin'
+email = 'admin@example.com'
+password = 'password'
+
+User = get_user_model()
+User.objects.create_superuser(username=username, email=email, password=password)
+EOF
 
 exec "$@"
