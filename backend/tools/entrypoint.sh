fe7e28a3ab856f27ad987a6a9a1aa60e9718c36f
sawamura-hayato <sunzecun@gmail.com>
2024-11-13 13:02:25 +0900

    :zap: スーパーユーザが既に存在していれば作成しない処理追加
    Volumeで保存しているため、再度コンテナを立ち上げたときにスーパーユーザーが存在してる可能性があるため

--- backend/tools/entrypoint.sh
+++ backend/tools/entrypoint.sh
@@ -6,12 +6,16 @@ python3 manage.py migrate --noinput
 python3 manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 
-username = 'admin'
-email = 'admin@example.com'
-password = 'password'
-
 User = get_user_model()
-User.objects.create_superuser(username=username, email=email, password=password)
+
+if not User.objects.filter(is_superuser=True).exists():
+	username = 'admin'
+	email = 'admin@example.com'
+	password = 'password'
+	User.objects.create_superuser(username=username, email=email, password=password)
+	print(f"Superuser '{username}' created successfully")
+else:
+	print(f"Superuser already exists")
 EOF
 
 exec "$@"
