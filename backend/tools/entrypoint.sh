9c06d544543e13a8ef909581f63e96ca228cd83d
sawamura-hayato <sunzecun@gmail.com>
2024-11-22 18:45:16 +0900

    :zap: docker-comopse.ymlの.envファイルから環境変数を取得し、SUPERUSERを作成

--- backend/tools/entrypoint.sh
+++ backend/tools/entrypoint.sh
@@ -3,18 +3,28 @@
 # todo: makemigrationsなど
 python3 manage.py migrate --noinput
 
-# todo: username, email. passwordの環境変数で登録
 python3 manage.py shell <<EOF
+import os
 from django.contrib.auth import get_user_model
+
+# 以下場合はエラー
+# - 環境変数が存在しない場合
+# - 空文字列の場合
+SUPERUSER_NAME = os.getenv('SUPERUSER_NAME')
+if not SUPERUSER_NAME:
+    raise ValueError("SUPERUSER_NAME is not set in environment variables")
+SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
+if not SUPERUSER_EMAIL:
+    raise ValueError("SUPERUSER_EMAIL is not set in environment variables")
+SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD')
+if not SUPERUSER_PASSWORD:
+    raise ValueError("SUPERUSER_PASSWORD is not set in environment variables")
 
 User = get_user_model()
 
 if not User.objects.filter(is_superuser=True).exists():
-	username = 'admin'
-	email = 'admin@example.com'
-	password = 'password'
-	User.objects.create_superuser(username=username, email=email, password=password)
-	print(f"Superuser '{username}' created successfully")
+	User.objects.create_superuser(username=SUPERUSER_NAME, email=SUPERUSER_EMAIL, password=SUPERUSER_PASSWORD)
+	print(f"Superuser '{SUPERUSER_NAME}' created successfully")
 else:
 	print(f"Superuser already exists")
 EOF
