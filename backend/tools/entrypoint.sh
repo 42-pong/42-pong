bc0a2674e9864a8240479028278eb967cf080b5a
sawamura-hayato <sunzecun@gmail.com>
2024-11-23 08:16:17 +0900

    :construction: get_env_valueで環境変数の値を取得し、SUPERUSER作成

--- backend/tools/entrypoint.sh
+++ backend/tools/entrypoint.sh
@@ -10,19 +10,6 @@ python3 manage.py shell <<EOF
 import os
 from django.contrib.auth import get_user_model
 
-# 以下場合はエラー
-# - 環境変数が存在しない場合
-# - 空文字列の場合
-SUPERUSER_NAME = os.getenv('SUPERUSER_NAME')
-if not SUPERUSER_NAME:
-    raise ValueError("SUPERUSER_NAME is not set in environment variables")
-SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
-if not SUPERUSER_EMAIL:
-    raise ValueError("SUPERUSER_EMAIL is not set in environment variables")
-SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD')
-if not SUPERUSER_PASSWORD:
-    raise ValueError("SUPERUSER_PASSWORD is not set in environment variables")
-
 User = get_user_model()
 
 if not User.objects.filter(is_superuser=True).exists():
@@ -31,6 +18,17 @@ if not User.objects.filter(is_superuser=True).exists():
         if not value:
             raise ValueError("{key} is not set in environment variables")
         return value
+
+    SUPERUSER_NAME = get_env_value("SUPERUSER_NAME")
+    SUPERUSER_EMAIL = get_env_value("SUPERUSER_EMAIL")
+    SUPERUSER_PASSWORD = get_env_value("SUPERUSER_PASSWORD")
+
+    User.objects.create_superuser(
+        username=SUPERUSER_NAME,
+        email=SUPERUSER_EMAIL,
+        password=SUPERUSER_PASSWORD,
+    )
+    print(f"Superuser '{SUPERUSER_NAME}' created successfully")
 else:
 	print(f"Superuser already exists")
 EOF
