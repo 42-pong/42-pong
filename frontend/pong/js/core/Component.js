05366f109d3f789747527a1fb1136ff2bda5b706
taekklee <taekklee@student.42tokyo.jp>
2024-12-10 17:49:37 +0900

    :seedling: (FE) 基盤クラス Component を追加

---
+++ frontend/pong/js/core/Component.js
@@ -0,0 +1,46 @@
+export class Component extends HTMLElement {
+  _eventListeners;
+  _state;
+
+  constructor(state = {}) {
+    super();
+    this._eventListeners = [];
+    this._state = state;
+  }
+
+  connectedCallback() {
+    this._render();
+  }
+
+  disconnectedCallback() {
+    for (const { eventType, handler } of this._eventListeners) {
+      this.removeEventListener(eventType, handler);
+    }
+    this._eventListeners = [];
+  }
+
+  _getState(key) {
+    return this._state[key];
+  }
+
+  _updateState(newState) {
+    Object.assign(this._state, newState);
+    this._render();
+  }
+
+  _template() {
+    return "";
+  }
+
+  _render() {
+    this.innerHTML = this._template();
+    this._afterRender();
+  }
+
+  _afterRender() {}
+
+  _attachEventListener(eventType, handler) {
+    this.addEventListener(eventType, handler);
+    this._eventListeners.push({ eventType, handler });
+  }
+}
