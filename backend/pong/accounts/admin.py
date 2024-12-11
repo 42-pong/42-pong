6a4666781a1a3fa75818d807bef763a851b553ad
habvi <93956895+habvi@users.noreply.github.com>
2024-12-12 07:19:11 +0900

    :zap:(accounts,admin) Django adminサイトでplayersテーブルを見れるように簡単に設定追加
    - adminサイトにAccountsが現れ,中にplayersテーブルが見られるようになる
    - 既存のauth_userもそのまま残っているはず

--- backend/pong/accounts/admin.py
+++ backend/pong/accounts/admin.py
@@ -1,3 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Player
+
+
+@admin.register(Player)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("created_at",)
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
