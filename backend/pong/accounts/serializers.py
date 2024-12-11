d7ce7db431f993a18dc9b96e51f9ddb082336293
habvi <93956895+habvi@users.noreply.github.com>
2024-12-12 08:02:39 +0900

    :zap:(accounts,serializer) Playerモデルのserializerのベースも作成し,Player->Userの順で作成してDBに保存
    - viewsからはPlayerSerializerのみ呼ばれるようにし,
      PlayerSerializerの中でUserSerializerを呼ぶようにした(この実装で良いのかは不明…)
    - create()は継承元のModelSerializerのメソッドをオーバーライドしている
      そのためAccountCreateViewでsave()が呼ばれると,内部的に自動でcreate()が呼ばれる
    - viewのResponse作成時に,playersテーブルに紐づいたuserテーブルの値を取得している(player.user.～)
    - swagger-uiでrequestを入力してユーザーを追加し,adminサイトで一覧が見られる

--- backend/pong/accounts/serializers.py
+++ backend/pong/accounts/serializers.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import Player
 
+
+# PlayerSerializerが呼ばれる時にこのUserSerializerも呼ばれる
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model: type[User] = User
@@ -15,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
             "password": {"write_only": True},
         }
 
-    # serializer.save()の内部で呼ばれる
+    # PlayerSerializerのuser_serializer.save()の内部で呼ばれる
     def create(self, validated_data: dict) -> User:
         # todo: usernameはBEが自動生成する
         user: User = User.objects.create_user(
@@ -24,3 +27,30 @@ class UserSerializer(serializers.ModelSerializer):
             password=validated_data["password"],
         )
         return user
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    # 同じ変数名(user)をfieldsに指定する必要がある
+    user: UserSerializer = UserSerializer()
+
+    class Meta:
+        model: type[Player] = Player
+        fields: tuple[str, ...] = ("id", "user", "created_at", "updated_at")
+        extra_kwargs: dict = {
+            "created_at": {"read_only": True},
+            "updated_at": {"read_only": True},
+        }
+
+    # AccountCreateViewのserializer.save()の内部で呼ばれる
+    def create(self, validated_data: dict) -> Player:
+        # requestの中にuserがあるので、それだけpopしてUserSerializerに渡す
+        user_data: dict = validated_data.pop("user")
+        user_serializer: UserSerializer = UserSerializer(data=user_data)
+
+        # UserSerializerのvalidate()が成功したらUserを作成する
+        user_serializer.is_valid(raise_exception=True)
+        new_user: User = user_serializer.save()
+
+        # Userを作成した後でPlayerを作成する
+        player: Player = Player.objects.create(user=new_user)
+        return player
--- backend/pong/accounts/views.py
+++ backend/pong/accounts/views.py
@@ -1,29 +1,37 @@
 from rest_framework import status
 
+# todo: IsAuthenticatedが追加されたらAllowAnyは不要かも
+from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSerializer
+from .models import Player
+from .serializers import PlayerSerializer
 
 
 class AccountCreateView(APIView):
+    serializer_class: type[PlayerSerializer] = PlayerSerializer
+    # todo: 認証機能を実装したら多分IsAuthenticatedに変更
+    permission_classes = (AllowAny,)
+
     # todo: try-exceptを書いて予期せぬエラー(実装上のミスを含む)の場合に500を返す
     def post(self, request: Request, *args: tuple, **kwargs: dict) -> Response:
         # requestをserializerに渡して変換とバリデーションを行う
-        serializer: UserSerializer = UserSerializer(data=request.data)
-        if not serializer.is_valid():
+        player_serializer = self.serializer_class(data=request.data)
+        if not player_serializer.is_valid():
             return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                player_serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
             )
 
-        # Userを新規作成してDBに追加し、作成された情報を返す
-        user = serializer.save()
+        # Account(PlayerとUser)を新規作成してDBに追加し、作成された情報を返す
+        player: Player = player_serializer.save()
         return Response(
             {
-                "id": user.id,
-                "username": user.username,
-                "email": user.email,
+                "player_id": player.id,
+                "username": player.user.username,
+                "email": player.user.email,
             },
             status=status.HTTP_201_CREATED,
         )
