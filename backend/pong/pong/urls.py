4f5ef3f111602673653de3b6fd2080f37d05084e
habvi <93956895+habvi@users.noreply.github.com>
2024-12-12 15:11:34 +0900

    :recycle:(swagger-ui) swagger-ui専用のimportをしなくてもincludeで統一できるので変更

--- backend/pong/pong/urls.py
+++ backend/pong/pong/urls.py
@@ -17,19 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from .health_check.views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # swagger-ui
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger-ui/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
+    path("api/schema/", include("swagger_ui.urls")),
     # health check
     path("api/health/", health_check, name="health-check"),
     # jwt_token
