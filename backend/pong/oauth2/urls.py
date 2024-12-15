e66b7b8cdd83d1bc3d8b0be511fc310c08a4ab00
Sawa <103574382+s1-haya@users.noreply.github.com>
2024-12-15 09:25:21 +0900

    :recycle: urlpatternsの命名をtokenからoauth2に変更
    Co-authored-by: habvi <93956895+habvi@users.noreply.github.com>

--- backend/pong/oauth2/urls.py
+++ backend/pong/oauth2/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import oauth2_authorize, oauth2_callback, oauth2_fetch_token
 
 urlpatterns = [
-    # 'api/token/'
-    path("authorize/", oauth2_authorize, name="token_authorize"),
-    path("callback/", oauth2_callback, name="token_callback"),
-    path("token/", oauth2_fetch_token, name="token"),
+    # 'api/oauth2/'
+    path("authorize/", oauth2_authorize, name="oauth2_authorize"),
+    path("callback/", oauth2_callback, name="oauth2_callback"),
+    path("token/", oauth2_fetch_token, name="oauth2_fetch_token"),
 ]
