26299a6b4f64b83ca1d6d9d4a3df97211ce01b25
habvi <93956895+habvi@users.noreply.github.com>
2024-12-16 04:17:25 +0900

    :zap:(accounts,unit) UserSerializerのエラーケースで必須fieldが空の場合のテスト追加

--- backend/pong/accounts/tests/serializers/test_user_serializer.py
+++ backend/pong/accounts/tests/serializers/test_user_serializer.py
@@ -24,7 +24,9 @@ class UserSerializerTests(TestCase):
             PASSWORD: "testpassword",
         }
 
+    # -------------------------------------------------------------------------
     # 正常ケース
+    # -------------------------------------------------------------------------
     def test_user_serializer_valid_data(self) -> None:
         """
         正常なデータが渡された場合にエラーにならないことを確認する
@@ -32,3 +34,36 @@ class UserSerializerTests(TestCase):
         serializer: UserSerializer = UserSerializer(data=self.user_data)
 
         self.assertTrue(serializer.is_valid())
+
+    # -------------------------------------------------------------------------
+    # エラーケース
+    # -------------------------------------------------------------------------
+    def test_user_serializer_empty_username(self) -> None:
+        """
+        必須フィールド"username"が空の場合にエラーになることを確認する
+        """
+        self.user_data[USERNAME] = ""
+        serializer: UserSerializer = UserSerializer(data=self.user_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(USERNAME, serializer.errors)
+
+    def test_user_serializer_empty_email(self) -> None:
+        """
+        必須フィールド"email"が空の場合にエラーになることを確認する
+        """
+        self.user_data[EMAIL] = ""
+        serializer: UserSerializer = UserSerializer(data=self.user_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(EMAIL, serializer.errors)
+
+    def test_user_serializer_empty_password(self) -> None:
+        """
+        必須フィールド"password"が空の場合にエラーになることを確認する
+        """
+        self.user_data[PASSWORD] = ""
+        serializer: UserSerializer = UserSerializer(data=self.user_data)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(PASSWORD, serializer.errors)
