4ea3068421a2039fac78e72136a0d289e788c012
habvi <93956895+habvi@users.noreply.github.com>
2024-12-16 04:10:43 +0900

    :seedling:(accounts,unit) UserSerializerの正常ケースのテスト追加

---
+++ backend/pong/accounts/tests/serializers/test_user_serializer.py
@@ -0,0 +1,34 @@
+from typing import Final
+
+from django.test import TestCase
+
+from ...constants import UserFields
+from ...serializers import UserSerializer
+
+ID: Final[str] = UserFields.ID
+USERNAME: Final[str] = UserFields.USERNAME
+EMAIL: Final[str] = UserFields.EMAIL
+PASSWORD: Final[str] = UserFields.PASSWORD
+
+
+class UserSerializerTests(TestCase):
+    def setUp(self) -> None:
+        """
+        TestCaseのsetUpメソッドのオーバーライド
+        各テストメソッドの実行前に毎回自動実行される
+        """
+        self.user_data: dict = {
+            ID: 1,
+            USERNAME: "testuser",
+            EMAIL: "testuser@example.com",
+            PASSWORD: "testpassword",
+        }
+
+    # 正常ケース
+    def test_user_serializer_valid_data(self) -> None:
+        """
+        正常なデータが渡された場合にエラーにならないことを確認する
+        """
+        serializer: UserSerializer = UserSerializer(data=self.user_data)
+
+        self.assertTrue(serializer.is_valid())
