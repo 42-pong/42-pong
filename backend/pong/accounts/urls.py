088adad32eab460f32aa2c6ead22c85315d8be2e
habvi <93956895+habvi@users.noreply.github.com>
2024-12-17 01:22:05 +0900

    :zap:(namespace) URLの逆引きをする際にnameが重複していても名前空間(app_name)を指定して区別できるように追加
    - 使い方 : url = reverse("app_name:name")
    - https://docs.djangoproject.com/ja/5.1/topics/http/urls/#url-namespaces

--- backend/pong/accounts/urls.py
+++ backend/pong/accounts/urls.py
@@ -1,7 +1,10 @@
+from typing import Final
+
 from django.urls import path
 
 from .views import AccountCreateView
 
+app_name: Final[str] = "accounts"
 urlpatterns = [
     # api/accounts/
     path("", AccountCreateView.as_view(), name="accounts")
