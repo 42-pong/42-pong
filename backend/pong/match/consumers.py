a83029a960a9869de6a42e90b2ed8f6f540c4637
ayase-mstk <masa1215.hayase@gmail.com>
2024-12-18 18:44:55 +0900

    :zap: (websocket) consumerを実装
    - websocketはクライアント一人につき一つしか作らないのでConsumerクラスは一つしか作らない。
    - イベントごとの処理はそれぞれハンドラクラスを作成しその中で行う。

--- backend/pong/match/consumers.py
+++ backend/pong/match/consumers.py
@@ -1,4 +1,24 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from . import match_handler
+
 class MultiEventConsumer(AsyncJsonWebsocketConsumer):
-    pass
+    async def connect(self):
+        # match用のハンドラを作成
+        self.match_handler = match_handler.MatchHandler()
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive_json(self, message):
+        category = message.get("category")
+        payload = message.get("payload")
+
+        match category:
+            case "MATCH":
+                # ここでマッチハンドラに処理移譲
+                pass
+            case _:
+                pass
