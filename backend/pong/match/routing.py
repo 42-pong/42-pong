3b503e61fa440e931b682a48a2bb2e53cba54bcd
ayase-mstk <masa1215.hayase@gmail.com>
2024-12-18 18:39:22 +0900

    :zap: (websocket) websocket接続のためのエンドポイント作成

--- backend/pong/match/routing.py
+++ backend/pong/match/routing.py
@@ -1,4 +1,7 @@
-from django.urls import re_path, path
+from django.urls import path
+
+from . import consumers
 
 websocket_urlpatterns = [
+    path("ws/match/", consumers.MultiEventConsumer.as_asgi())
 ]
