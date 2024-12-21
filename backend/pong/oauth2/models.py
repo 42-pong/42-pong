94668caa764a3722193c6759301116835fefcd94
s1-haya <sunzecun@gmail.com>
2024-12-21 12:43:43 +0900

    :zap: FortyTwoTokenのモデル作成
    Userモデル : Djangoのデフォルトのユーザーモデル
    OAuth2モデル : OAuth2認証のサービスモデル
    FortyTwoTokenモデル : : 42ユーザーのトークン情報を管理するモデル

--- backend/pong/oauth2/models.py
+++ backend/pong/oauth2/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class FortyTwoToken(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="forty_two_token"
+    )
+    access_token = models.CharField(max_length=255, unique=True)
+    # 42はbearerとmacのみなので6文字で固定
+    token_type = models.CharField(max_length=6)
+    access_token_expiry = models.DateTimeField()
+    refresh_token = models.CharField(max_length=255, unique=True)
+    refresh_token_expiry = models.DateTimeField()
+    scope = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "forty_two_tokens"
+
+    def __str__(self) -> str:
+        return self.access_token
