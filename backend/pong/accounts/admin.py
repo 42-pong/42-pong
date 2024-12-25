6f72e1d997e600d75bd36f3fa86fb8ed64ed2a02
habvi <93956895+habvi@users.noreply.github.com>
2024-12-26 05:20:56 +0900

    :recycle:(accounts,admin) adminサイトのACCOUNTS/Playersに、Playerに1対1で紐づくUserのuser_idとusernameを表示するように

--- backend/pong/accounts/admin.py
+++ backend/pong/accounts/admin.py
@@ -6,14 +6,33 @@ from .models import Player
 
 @admin.register(Player)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = (
-        PlayerFields.ID,
-        PlayerFields.USER,
+    """
+    adminサイトでPlayersに表示されるカラムをカスタマイズ
+    """
+
+    # Playerに紐づくUserの情報をadminサイトで表示する為にはメソッド名をそのまま指定
+    # メソッド名はアンダースコアがスペースに変換されてカラム名として扱われる
+    # todo: Player特有のfieldが追加されたらそれらも追加する
+    list_display: tuple = (
+        "user_id",
+        "username",
         PlayerFields.CREATED_AT,
         PlayerFields.UPDATED_AT,
     )
-    list_filter = (PlayerFields.UPDATED_AT,)
-    search_fields = (
+    list_filter: tuple = (PlayerFields.UPDATED_AT,)
+    search_fields: tuple = (
         f"{PlayerFields.USER}__{UserFields.USERNAME}",
         f"{PlayerFields.USER}__{UserFields.EMAIL}",
     )
+
+    def user_id(self, obj: Player) -> int:
+        """
+        Playerと1対1関係にあるUserのIDをlist_displayで表示するためのメソッド
+        """
+        return obj.user.id
+
+    def username(self, obj: Player) -> str:
+        """
+        Playerと1対1関係にあるUserのusernameをlist_displayで表示するためのメソッド
+        """
+        return obj.user.username
