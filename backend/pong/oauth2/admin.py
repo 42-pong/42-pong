1643e9b543fc6bc0942de1076060192872d5f4d6
s1-haya <sunzecun@gmail.com>
2024-12-27 11:06:59 +0900

    :zap: FortyTwoTokenAdmin作成
    has_add_permissionとhas_change_permissionはadmin.ModelAdminクラスメソッドのオーバライド
    https://docs.djangoproject.com/en/5.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.has_add_permission

--- backend/pong/oauth2/admin.py
+++ backend/pong/oauth2/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FortyTwoToken
+
+
+@admin.register(FortyTwoToken)
+class FortyTwoTokenAdmin(admin.ModelAdmin):
+    # トークン情報の漏れを防ぐため、アクセストークンとリフレッシュトークンを公開しない
+    exclude = ("access_token", "refresh_token")
+    list_display = (
+        "user",
+        "access_token_expiry",
+        "created_at",
+    )
+    list_filter = ("created_at",)
+
+    def has_add_permission(self, request) -> bool:
+        """
+        管理サイトでのFortyTwoTokenAdminのインスタンスを追加する権限を拒否します。
+        """
+        return False
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        """
+        管理サイトでのFortyTwoTokenAdminのインスタンスを変更する権限を拒否します。
+        """
+        return False
