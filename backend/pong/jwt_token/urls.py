fd816c915c4277518faacfc29188fdaa05e4e66c
habvi <93956895+habvi@users.noreply.github.com>
2024-12-30 00:18:35 +0900

    :recycle:(jwt_token) 1つ上の名前空間までimportして使用

--- backend/pong/jwt_token/urls.py
+++ backend/pong/jwt_token/urls.py
@@ -1,16 +1,12 @@
 from typing import Final
 
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+from rest_framework_simplejwt import views
 
 app_name: Final[str] = "jwt_token"
 urlpatterns = [
     # 'api/token/'
-    path("", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("", views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh/", views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", views.TokenVerifyView.as_view(), name="token_verify"),
 ]
