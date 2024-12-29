a77e063b1c395a4c36c14b04981bd6db1a46d00f
habvi <93956895+habvi@users.noreply.github.com>
2024-12-30 04:31:20 +0900

    :recycle:(accounts) UserとPlayerを紐づけてPlayerSerializerを作成する部分を関数分け
    - create_account()ではUserのsave()とPlayerのsave()だけを主にしたいため

--- backend/pong/accounts/create_account.py
+++ backend/pong/accounts/create_account.py
@@ -5,7 +5,35 @@ import utils.result
 
 from . import constants, models, serializers
 
+CreatePlayerSerializerResult = utils.result.Result[
+    serializers.PlayerSerializer, dict
+]
 CreateAccountResult = utils.result.Result[models.Player, dict]
+
+
+def _create_user_related_player_serializer(
+    player_data: dict, user_id: int
+) -> CreatePlayerSerializerResult:
+    """
+    player_dataとUserを紐づけ、PlayerSerializerを新規作成する
+
+    Args:
+        user_id: UserのPK
+        player_data: PlayerSerializerに渡すdata
+
+    Returns:
+        CreatePlayerSerializerResult: PlayerSerializerのResult
+    """
+    # PKであるuser.idを"user"フィールドにセットしUserとPlayerを紐づける
+    player_data[constants.PlayerFields.USER] = user_id
+
+    # PlayerSerializer作成
+    player_serializer: serializers.PlayerSerializer = (
+        serializers.PlayerSerializer(data=player_data)
+    )
+    if not player_serializer.is_valid():
+        return CreatePlayerSerializerResult.error(player_serializer.errors)
+    return CreatePlayerSerializerResult.ok(player_serializer)
 
 
 # todo: トランザクションの処理が必要。User,Playerのどちらかが作成されなかった場合はロールバック
@@ -22,14 +50,20 @@ def create_account(
     # User作成
     user: User = user_serializer.save()
 
-    # User作成の後にPlayer作成
-    # PKであるuser.idを"user"フィールドにセットしUserとPlayerを紐づける
-    player_data[constants.PlayerFields.USER] = user.id
-    player_serializer: serializers.PlayerSerializer = (
-        serializers.PlayerSerializer(data=player_data)
+    # app間で共通のPlayerSerializer作成
+    player_serializer_result: CreatePlayerSerializerResult = (
+        _create_user_related_player_serializer(player_data, user.id)
     )
-    if not player_serializer.is_valid():
-        return CreateAccountResult.error(player_serializer.errors)
+    if player_serializer_result.is_error:
+        return CreateAccountResult.error(
+            player_serializer_result.unwrap_error()
+        )
+    player_serializer: serializers.PlayerSerializer = (
+        player_serializer_result.unwrap()
+    )
+
+    # User作成の後にPlayer作成
     player: models.Player = player_serializer.save()
+
     # todo: userを返すか、accountクラスなどに詰めて返す
     return CreateAccountResult.ok(player)
