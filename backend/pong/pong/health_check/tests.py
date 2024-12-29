5693b06bda9e4b1f387904c2539cfbcdecd240db
habvi <93956895+habvi@users.noreply.github.com>
2024-12-30 00:20:19 +0900

    :recycle:(health_check) 1つ上の名前空間までimportして使用

--- backend/pong/pong/health_check/tests.py
+++ backend/pong/pong/health_check/tests.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
-from rest_framework import status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.test import APIRequestFactory
+from rest_framework import request as drf_request
+from rest_framework import response as drf_response
+from rest_framework import status, test
 
-from .views import health_check
+from . import views
 
 
 class HealthCheckUnitTests(TestCase):
@@ -13,9 +12,9 @@ class HealthCheckUnitTests(TestCase):
         Unit tes for health_check view.
         GET /health/ returns 200 OK
         """
-        factory: APIRequestFactory = APIRequestFactory()
-        request: Request = factory.get("/health/")
-        response: Response = health_check(request)
+        factory: test.APIRequestFactory = test.APIRequestFactory()
+        request: drf_request.Request = factory.get("/health/")
+        response: drf_response.Response = views.health_check(request)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"status": "OK"})
