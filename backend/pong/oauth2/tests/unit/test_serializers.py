15703d4208c66585181d203dfa6738d7d1ff72ba
s1-haya <sunzecun@gmail.com>
2025-01-06 17:19:50 +0900

    :zap: (BE:oauth2)UserSerializerが正しく機能するかを確認するテスト作成

--- backend/pong/oauth2/tests/unit/test_serializers.py
+++ backend/pong/oauth2/tests/unit/test_serializers.py
@@ -5,10 +5,49 @@ from oauth2 import serializers
 
 class UserSerializerTestCase(TestCase):
     def setUp(self) -> None:
-        self.Serializer = serializers.UserSerializer
+        """
+        unittest.TestCaseのsetUpメソッドをオーバーライドして、テスト実行前に必要な初期設定を行う関数
 
-    def test_true(self) -> None:
-        self.assertTrue(True)
+        初期設定
+        - self.SerializerにUserSerializerのインスタンスを代入
+        """
+        self.Serializer: serializers.UserSerializer = (
+            serializers.UserSerializer
+        )
+
+    # todo: パスワードに文字列を入力した場合のテストを書く?（現状だとパスワードの値は入る）
+    #       その場合は関数名を変更する。
+    def test_valid_serializer(self) -> None:
+        """
+        正しいデータが与えられた場合、UserSerializerが正しく機能するかを確認するテスト
+
+        テスト項目
+        - シリアライザが有効であること
+        - 検証されたデータが期待通りであること
+        - シリアライズされたデータが期待通りであること
+        - シリアライザにエラーがないこと
+        """
+
+        # passwordが空文字を想定
+        user_data: dict = {
+            "id": 1,
+            "username": "pong",
+            "email": "pong@gmail.com",
+            "password": "",
+        }
+        serializer: serializers.UserSerializer = self.Serializer(
+            data=user_data
+        )
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(
+            serializer.validated_data,
+            {"username": "pong", "email": "pong@gmail.com", "password": ""},
+        )
+        self.assertEqual(
+            serializer.data, {"username": "pong", "email": "pong@gmail.com"}
+        )
+        self.assertTrue(serializer.errors == {})
+
 
 
 class OAuth2SerializerTestCase(TestCase):
