b4f4d6d26bad6b5165f40f667ef414e6b99539f6
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-07 19:51:22 +0900

    :recycle: (websocket/match) enumの共通処理を基底クラスに集約

--- backend/pong/match/match_enums.py
+++ backend/pong/match/match_enums.py
@@ -2,41 +2,31 @@ from enum import Enum
 from typing import Final
 
 
-class Stage(Enum):
+class BaseEnum(Enum):
+    @classmethod
+    def key(cls) -> str:
+        return cls.__name__.lower()
+
+
+class Stage(BaseEnum):
     INIT: Final[str] = "INIT"
     READY: Final[str] = "READY"
     PLAY: Final[str] = "PLAY"
     END: Final[str] = "END"
     NONE: Final[str] = "NONE"
 
-    @classmethod
-    def key(cls) -> str:
-        return "stage"
 
-
-class Mode(Enum):
+class Mode(BaseEnum):
     LOCAL: Final[str] = "local"
     REMOTE: Final[str] = "remote"
 
-    @classmethod
-    def key(cls) -> str:
-        return "mode"
 
-
-class Team(Enum):
+class Team(BaseEnum):
     One: Final[str] = "1"
     Two: Final[str] = "2"
     Empty: Final[str] = ""
 
-    @classmethod
-    def key(cls) -> str:
-        return "team"
 
-
-class Move(Enum):
+class Move(BaseEnum):
     UP: Final[str] = "UP"
     DOWN: Final[str] = "DOWN"
-
-    @classmethod
-    def key(cls) -> str:
-        return "move"
