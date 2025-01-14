502f34ec60e39336068d69754f4dcc2d7d98a555
taekklee <taekklee@student.42tokyo.jp>
2025-01-14 11:12:00 +0900

    :seedling: (FE:Chat) 空の ChatView を作成

--- frontend/pong/js/components/index.js
+++ frontend/pong/js/components/index.js
@@ -1,5 +1,7 @@
+import { ChatView } from "./views/ChatView";
 import { HomeView } from "./views/HomeView";
 import { LoginView } from "./views/LoginView";
 
+customElements.define("chat-view", ChatView, {});
 customElements.define("home-view", HomeView, {});
 customElements.define("login-view", LoginView, {});
