bf7e98e56c1dea1c54d89e1b600e26958bdd4c22
taekklee <taekklee@student.42tokyo.jp>
2025-01-14 11:37:26 +0900

    :seedling: (FE:MainNav) 仮のナビゲーションを作成

--- frontend/pong/js/components/index.js
+++ frontend/pong/js/components/index.js
@@ -1,7 +1,9 @@
+import { MainNav } from "./navs/MainNav";
 import { ChatView } from "./views/ChatView";
 import { HomeView } from "./views/HomeView";
 import { LoginView } from "./views/LoginView";
 
+customElements.define("main-nav", MainNav, {});
 customElements.define("chat-view", ChatView, {});
 customElements.define("home-view", HomeView, {});
 customElements.define("login-view", LoginView, {});
