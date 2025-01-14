dfa8f7a9775f853e9e12b459352a5542577e1647
taekklee <taekklee@student.42tokyo.jp>
2025-01-14 12:03:32 +0900

    :seedling: (FE:MainView) ルーティング操作を二段階に分割

--- frontend/pong/js/components/index.js
+++ frontend/pong/js/components/index.js
@@ -2,8 +2,10 @@ import { MainNav } from "./navs/MainNav";
 import { ChatView } from "./views/ChatView";
 import { HomeView } from "./views/HomeView";
 import { LoginView } from "./views/LoginView";
+import { MainView } from "./views/MainView";
 
 customElements.define("main-nav", MainNav, {});
 customElements.define("chat-view", ChatView, {});
 customElements.define("home-view", HomeView, {});
 customElements.define("login-view", LoginView, {});
+customElements.define("main-view", MainView, {});
--- frontend/pong/js/routers/appRouter.js
+++ frontend/pong/js/routers/appRouter.js
@@ -1,12 +1,12 @@
-import { HomeView } from "../components/views/HomeView";
-import { LoginView } from "../components/views/LoginView";
+import { MainView } from "../components/views/MainView";
+import { Paths } from "../constants/Paths";
 import { Route } from "../core/Route";
 import { Router } from "../core/Router";
 
 export const appRouter = (target) => {
   const routes = {
-    "/": Route.defaultRoute(HomeView),
-    "/login": Route.defaultRoute(LoginView),
+    [Paths.HOME]: Route.createRoute(MainView, MainView.Paths.HOME),
+    [Paths.CHAT]: Route.createRoute(MainView, MainView.Paths.CHAT),
   };
-  return new Router(target, HomeView, routes);
+  return new Router(target, MainView, routes);
 };
