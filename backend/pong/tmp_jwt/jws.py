71456e732dc02997b6267f798e06cfae4eafc307
s1-haya <sunzecun@gmail.com>
2025-01-20 17:54:39 +0900

    :zap: (BE:jwt/jws.py) sign HMACを使ってシグネチャ作成する関数作成
    ハッシュアルゴリズムはsha256を指定しています
    
    sha256
    secure hash algorithmの略で、長さが256ビットの値を返してくれるハッシュ関数
    
    HMAC
    Hash-based Message Authentication Codeの略でメッセージが改ざんされてないことを確認するための情報。
    その情報の生成をハッシュ関数で行うのがHMAC
    https://www.notion.so/BE-jwt-HMAC-cd5433095f89444dbeb8958135740db3

--- backend/pong/tmp_jwt/jws.py
+++ backend/pong/tmp_jwt/jws.py
@@ -3,12 +3,16 @@
 # - JWS(RFC7515)の概要を読む
 # - DRFを使ってどうユーザーの認証を行うのかについて
 # 実装
-# - 署名する関数実装
 #   def verify_jws(self, jws: str) -> bool:
 # - 検証する関数実装
 # - JWSのペイロード内の`exp`クレームを検証して、有効期限を確認するを実装
 #   def validate_jws_expiration(self, payload: dict) -> bool:
+import hashlib
+import hmac
+
 from pong import settings
+
+from . import base64_url
 
 
 class JWS:
@@ -21,8 +25,23 @@ class JWS:
     """
 
     def __init__(self) -> None:
+        self.base64_url_handler = base64_url.Base64Url()
         self.secret_key = settings.JWS_SECRET_KEY
+
         if not self.secret_key:
             raise ValueError(
                 "JWS_SECRET_KEY is not defined in the environment variables."
             )
+
+    def sign(self, header_encoded: str, payload_encoded: str) -> str:
+        """エンコードされたヘッダーとペイロードを基に、エンコードされたシグネチャを生成する"""
+        signing_input: str = f"{header_encoded}.{payload_encoded}"
+        # ハッシュアルゴリズムを引数で受け取る場合はValueErrorを対応する必要あり
+        # https://github.com/python/cpython/blob/main/Lib/hmac.py#L38
+        signature: bytes = hmac.new(
+            self.secret_key.encode("utf-8"),
+            signing_input.encode("utf-8"),
+            hashlib.sha256,
+        ).digest()
+        signature_encoded = self.base64_url_handler.encode(signature)
+        return signature_encoded
