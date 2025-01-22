0ed8bf1c4b2211e39fbcc253512297c3c77b84fa
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-22 12:57:22 +0900

    :zap: (tournaments/participation) 仮のSerializerを定義
    - ModelViewSetの定義でシリアライザ―が必要なため

---
+++ backend/pong/tournaments/participation/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class ParticipationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Participation
+        fields = "__all__"
+
+    # TODO: rankingに渡される値が正確かどうかのバリデーション実装
+    # TODO: participation_nameが空だったらエラー
