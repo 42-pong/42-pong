fbed9d5935de778c7cba6db36e86aee88044f872
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-22 13:06:26 +0900

    :zap: (tournaments/participation) ルーティングを追加

--- backend/pong/tournaments/urls.py
+++ backend/pong/tournaments/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .participation import views as participation_views
 from .tournament import views as tournament_views
 
 router = routers.DefaultRouter()
+# この順番でregisterしないとparticipationsのエンドポイントのURIマッチングが通らない
+router.register(r"participations", participation_views.ParticipationViewSet)
 router.register(r"", tournament_views.TournamentViewSet)
 
 urlpatterns = [
