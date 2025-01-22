19e22d7050b07bb7a67fff66a3238554105b089e
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-22 12:58:32 +0900

    :zap: (tournaments/participation) ModelViewSetを継承したViewクラスのエンドポイントをOpenAPIで記述

---
+++ backend/pong/tournaments/participation/views.py
@@ -0,0 +1,52 @@
+from drf_spectacular.utils import (
+    OpenApiParameter,
+    extend_schema,
+    extend_schema_view,
+)
+from rest_framework import viewsets
+
+from . import models, serializers
+
+
+@extend_schema_view(
+    # requestやresponse, exampleは後で追記
+    list=extend_schema(
+        description="Participationレコード一覧を取得する。クエリパラメータによるフィルタリングも可能。",
+        operation_id="participations_list",  # 明示的にoperationIdを設定
+        parameters=[
+            OpenApiParameter(
+                name="player-id",
+                description="playerテーブルのID",
+                required=False,
+                type=int,
+                location=OpenApiParameter.QUERY,
+            ),
+            OpenApiParameter(
+                name="tournament-id",
+                description="TournamentテーブルのID",
+                required=False,
+                type=int,
+                location=OpenApiParameter.QUERY,
+            ),
+        ],
+    ),
+    retrieve=extend_schema(
+        description="指定されたIDのParticipationレコードの詳細を取得する。",
+        operation_id="tournaments_retrieve_by_id",  # 明示的にoperationIdを設定
+    ),
+    create=extend_schema(
+        description="新しいParticipationレコードを作成する。",
+    ),
+    partial_update=extend_schema(
+        description="指定されたIDのParticipationレコードのランキングを登録します。",
+    ),
+    destroy=extend_schema(
+        description="指定されたIDのParticipationレコードを削除する。",
+    ),
+)
+class ParticipationViewSet(viewsets.ModelViewSet):
+    queryset = models.Participation.objects.all()
+    serializer_class = serializers.ParticipationSerializer
+
+    # 使用できるHTTPメソッドを制限
+    http_method_names = ["get", "post", "patch", "delete"]
