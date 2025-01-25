400bc91c4fe44a40321979e0b68a169256231996
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-25 22:50:12 +0900

    :zap: (ws) websocketのendpointを'ws/'に変更
    - 'ws/match/'だとmatchにしか使わないように感じるので、'ws/'に変更

--- backend/pong/ws/routing.py
+++ backend/pong/ws/routing.py
@@ -2,6 +2,4 @@ from django.urls import path
 
 from . import consumers
 
-websocket_urlpatterns = [
-    path("ws/match/", consumers.MultiEventConsumer.as_asgi())
-]
+websocket_urlpatterns = [path("ws/", consumers.MultiEventConsumer.as_asgi())]
