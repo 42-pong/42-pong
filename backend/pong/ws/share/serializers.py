fb32b045987bc6c79cd07703cd9c2ce248a66d9b
ayase-mstk <masa1215.hayase@gmail.com>
2025-01-25 22:48:01 +0900

    :recycle: (ws) ディレクトリ構成に沿ってimportパスを修正

--- backend/pong/ws/share/serializers.py
+++ backend/pong/ws/share/serializers.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from rest_framework import serializers
 
-from .. import ws_constants
+from . import constants as ws_constants
 
 
 class BaseWebsocketSerializer(serializers.Serializer):
