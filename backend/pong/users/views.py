ee107a33eead2278792d3def54cce74a4908b823
habvi <93956895+habvi@users.noreply.github.com>
2025-01-27 04:54:29 +0900

    :zap:(users) /api/users/,GETの200のextend_schemaを作成

--- backend/pong/users/views.py
+++ backend/pong/users/views.py
@@ -1,4 +1,6 @@
+from drf_spectacular import utils
 from rest_framework import (
+    permissions,
     request,
     response,
     status,
@@ -13,6 +15,39 @@ class UsersListView(views.APIView):
     ユーザープロフィールの一覧を取得するビュー
     """
 
+    # todo: IsAuthenticatedに変更する
+    permission_classes = (permissions.AllowAny,)
+
+    @utils.extend_schema(
+        request=None,
+        responses={
+            200: utils.OpenApiResponse(
+                description="A list of user profiles",
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    utils.OpenApiExample(
+                        "Example 200 response",
+                        value={
+                            "status": "ok",
+                            "data": [
+                                {
+                                    "id": 2,
+                                    "username": "username1",
+                                    # todo: display_name,avatar追加
+                                },
+                                {
+                                    "id": 3,
+                                    "username": "username2",
+                                    # todo: display_name,avatar追加
+                                },
+                                {"...", "..."},
+                            ],
+                        },
+                    ),
+                ],
+            ),
+        },
+    )
     def get(self, request: request.Request) -> response.Response:
         """
         ユーザープロフィール一覧を取得するGETメソッド
