c214ae1e1093ca4922ad7765d0e191d4c3ecb915
habvi <93956895+habvi@users.noreply.github.com>
2025-01-27 04:48:26 +0900

    :zap:(users) ユーザープロフィール一覧を取得するviewのベース作成

--- backend/pong/users/views.py
+++ backend/pong/users/views.py
@@ -1,3 +1,20 @@
-# from django.shortcuts import render
+from rest_framework import (
+    request,
+    response,
+    status,
+    views,
+)
 
-# Create your views here.
+from pong.custom_response import custom_response
+
+
+class UsersListView(views.APIView):
+    """
+    ユーザープロフィールの一覧を取得するビュー
+    """
+
+    def get(self, request: request.Request) -> response.Response:
+        """
+        ユーザープロフィール一覧を取得するGETメソッド
+        """
+        return custom_response.CustomResponse(status=status.HTTP_200_OK)
