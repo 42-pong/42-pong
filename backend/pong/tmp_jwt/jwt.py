7df7cecb886e5c272b81b4896aac300b77039990
s1-haya <sunzecun@gmail.com>
2025-01-27 12:39:06 +0900

    :zap: (BE:jwt) JWT.encode payloadから署名済みのJWTを生成するとテスト関数作成
    テストはunittest.mock.dispatchを使ってsign関数をmockした
    dispatchは関数デコレーターを定義した。デコレーターとは関数やクラスの振る舞いを変更または拡張するための仕組みのこと
    MagicMockクラスはMockクラスのサブクラスで、Mockクラスの機能に加えて特殊メソッド（MagicMethod）のMock機能を持つ
    
    unittest.mock
    https://docs.python.org/ja/3.13/library/unittest.mock.html#the-patchers
    
    MagicMock
    https://docs.python.org/ja/3.13/library/unittest.mock.html#unittest.mock.MagicMock

--- backend/pong/tmp_jwt/jwt.py
+++ backend/pong/tmp_jwt/jwt.py
@@ -7,9 +7,7 @@
 # - JWTを生成する関数実装
 # - payloadを取得する関数実装
 
-from typing import Optional
-
-from . import jws
+from . import base64_url, jws
 
 
 class JWT:
@@ -25,7 +23,7 @@ class JWT:
     https://datatracker.ietf.org/doc/html/rfc7519
     """
 
-    def __init__(self, payload: Optional[dict] = None) -> None:
+    def __init__(self) -> None:
         """
         JWTクラスのインスタンスを初期化。
 
@@ -34,5 +32,45 @@ class JWT:
         """
         # todo: アルゴリズムも選べるようにする？
         self.header: dict = {"typ": "JWT", "alg": "HS256"}
-        self.payload: dict = payload or {}
         self.jws: jws.JWS = jws.JWS()
+        self.base64_url_handler: base64_url.Base64Url = base64_url.Base64Url()
+
+    def encode(self, payload: dict) -> str:
+        """
+        引数で渡されたペイロードを検証し、署名済みのJWTを生成します。
+
+        Args:
+            todo: ペイロードの詳細な構造を記述
+            payload (dict)
+                - 必須フィールド: ...
+                - 任意フィールド: ...
+        Returns:
+            str: 署名済みのJWT
+
+        Raises:
+            ValueError:
+            todo: validate_payload関数作成
+            - ペイロードが空である場合
+            - ペイロード内の必須フィールドが不足している場合
+            - ...
+
+            sign
+            - 有効な文字: 英字 (A-Z, a-z), 数字 (0-9), ハイフン (-), アンダースコア (_) 以外の文字が含まれている場合
+        """
+        # todo: payloadの検証
+        encoded_header: str = self.base64_url_handler.encode_dict(self.header)
+        encoded_payload: str = self.base64_url_handler.encode_dict(payload)
+        encoded_signature: str = self.jws.sign(encoded_header, encoded_payload)
+        return f"{encoded_header}.{encoded_payload}.{encoded_signature}"
+
+    # @staticmethod
+    # def decode(token: str, secret_key: str) -> dict:
+    #     """
+    #     JWTトークンをデコードする。
+    #     """
+    #     try:
+    #         return jwt.decode(token, secret_key, algorithms=["HS256"])
+    #     except jwt.ExpiredSignatureError:
+    #         raise ValueError("JWTの有効期限が切れています。")
+    #     except jwt.InvalidTokenError:
+    #         raise ValueError("無効なJWTトークンです。")
--- backend/pong/tmp_jwt/tests/unit/test_jwt.py
+++ backend/pong/tmp_jwt/tests/unit/test_jwt.py
@@ -1,8 +1,34 @@
+from unittest.mock import MagicMock, patch
+
 from django.test import TestCase
 
-from tmp_jwt import jwt
+from tmp_jwt import jws, jwt
 
 
 class JsonWebTokenFunctionTestCase(TestCase):
     def setUp(self) -> None:
         self.jwt_handler: jwt.JWT = jwt.JWT()
+
+    @patch.object(
+        jws.JWS,
+        "sign",
+        # sign関数の引数にexpected_encoded_headerとexpected_encoded_payloadを渡した返り値
+        return_value="wELfrcYgcf8pyBxSMOCRINRj8QXlxP360D0T3E_bq3U",
+    )
+    def test_encode(
+        self,
+        mock_sign: MagicMock,
+    ) -> None:
+        # {"typ": "JWT", "alg": "HS256"}をBase64エンコード
+        expected_encoded_header: str = (
+            "eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJIUzI1NiJ9"
+        )
+        # {"sub": "1234567890"}のBase64エンコード
+        expected_encoded_payload: str = "eyJzdWIiOiAiMTIzNDU2Nzg5MCJ9"
+        expected_encoded_signature: str = mock_sign.return_value
+        payload: dict = {"sub": "1234567890"}
+        result: str = self.jwt_handler.encode(payload)
+        self.assertEqual(
+            result,
+            f"{expected_encoded_header}.{expected_encoded_payload}.{expected_encoded_signature}",
+        )
