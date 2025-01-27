c86c86a60029ef6d89bf63e14a28201ab92d4b2c
taekklee <taekklee@student.42tokyo.jp>
2025-01-28 00:22:30 +0900

    :seedling: (FE:MainNav)  スタイリングの更新

--- frontend/pong/js/components/navs/MainNav.js
+++ frontend/pong/js/components/navs/MainNav.js
@@ -1,7 +1,7 @@
 import { Paths } from "../../constants/Paths";
 import { PongEvents } from "../../constants/PongEvents";
 import { Component } from "../../core/Component";
-import { createElement } from "../../utils/elements/createElement";
+import { createDefaultNavbar } from "../../utils/elements/nav/createDefaultNavbar";
 
 export class MainNav extends Component {
   #nav;
@@ -15,27 +15,7 @@ export class MainNav extends Component {
   ]);
 
   _onConnect() {
-    const ul = getNavItemsList(MainNav.links);
-
-    this.#nav = createElement("nav");
-    // TODO: [DELETE] 一時的なスタイル指定
-    this.#nav.innerHTML += `
-    <style>
-      nav {
-        border-top: 1px solid black;
-        border-bottom: 1px solid black;
-      }
-      nav ul{
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-      }
-      nav ul li{
-        list-style: none;
-        margin: 5px;
-      }
-    </style>`;
-    this.#nav.appendChild(ul);
+    this.#nav = createDefaultNavbar(MainNav.links);
 
     this._attachEventListener("click", (event) => {
       event.preventDefault();
@@ -52,25 +32,3 @@ export class MainNav extends Component {
     this.appendChild(this.#nav);
   }
 }
-
-const getNavItemsList = (links) => {
-  const ul = createElement("ul");
-
-  for (const link of MainNav.links) {
-    const anchor = createElement(
-      "a",
-      {
-        textContent: link.name,
-      },
-      {
-        href: link.path,
-      },
-    );
-
-    const li = createElement("li");
-    li.appendChild(anchor);
-
-    ul.appendChild(li);
-  }
-  return ul;
-};
