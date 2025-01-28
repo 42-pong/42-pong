08ea8764f6a54fcc677fccbb8b35adbc3e8feef9
s1-haya <sunzecun@gmail.com>
2025-01-28 11:46:31 +0900

    :zap: (BE:jwt) encode_dict関数のテストケース追加
    encode_dict関数のテストということでTypeErrorのテストケースを書いたが、ペイロードの検証を行うときに弾くのでJWTには影響はないです
    
    json.dumps
    https://docs.python.org/ja/3.13/library/json.html#json.dumps

--- backend/pong/tmp_jwt/tests/unit/test_base64_url.py
+++ backend/pong/tmp_jwt/tests/unit/test_base64_url.py
@@ -1,3 +1,4 @@
+import parameterized  # type: ignore[import-untyped]
 from django.test import TestCase
 
 from tmp_jwt import base64_url
@@ -20,8 +21,48 @@ class Base64UrlTestCase(TestCase):
         """空のデータをエンコードした場合、str型の空文字を返すことを確認するテスト"""
         empty_data: bytes = b""
         expected_encoded_data: str = ""
-        encode_data: str = self.base64_url_handler.encode_bytes(empty_data)
-        self.assertEqual(encode_data, expected_encoded_data)
+        encoded_data: str = self.base64_url_handler.encode_bytes(empty_data)
+        self.assertEqual(encoded_data, expected_encoded_data)
+
+    @parameterized.parameterized.expand(
+        [
+            (
+                "JWTにあるヘッダーを想定するdict型のデータ",
+                {"typ": "JWT", "alg": "HS256"},
+                "eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJIUzI1NiJ9",
+            ),
+            (
+                "JWTにあるペイロードーを想定するdict型のデータ",
+                {"sub": "1234567890"},
+                "eyJzdWIiOiAiMTIzNDU2Nzg5MCJ9",
+            ),
+            # dump()だとValueErrorの例外を投げるが、dumps()だとobjをJSON形式のstrオブジェクトに直列化するため例外を投げない想定
+            # 詳細 https://docs.python.org/ja/3.13/library/json.html#json.dumps
+            (
+                "dump()だとValueErrorを投げるdict型のデータ",
+                {"key": float("nan")},
+                "eyJrZXkiOiBOYU59",
+            ),
+        ]
+    )
+    def test_encode_dict(
+        self, testcase_name: str, data: dict, expected_encoded_data: str
+    ) -> None:
+        """正常なデータがBase64でエンコードされるかをテスト
+
+        Args:
+            testcase_name: テストケースの説明
+            data: dict型のデータ
+            expected_encoded_data: 期待するエンコードしたdata
+        """
+        encoded_data: str = self.base64_url_handler.encode_dict(data)
+        self.assertEqual(expected_encoded_data, encoded_data)
+
+    def test_encode_dict_invalid(self) -> None:
+        """シリアライズ可能なJSON形式ではないdict型のデータの場合、TypeErrorかどうかを確認するテスト"""
+        data: dict = {"key": {1, 2, 3}}
+        with self.assertRaises(TypeError):
+            self.base64_url_handler.encode_dict(data)
 
     def test_decode_bytes(self) -> None:
         """
@@ -53,8 +94,8 @@ class Base64UrlTestCase(TestCase):
         with self.assertRaises(ValueError):
             self.base64_url_handler.decode_bytes(invalid_urlsafe_base64_data)
 
-    def test_encode_decode_round_trip(self) -> None:
-        """Base64エンコードしたデータをデコードして元のデータに戻ることを確認するテスト"""
+    def test_encode_decode_bytes_round_trip(self) -> None:
+        """Base64URLセーフ形式にbytes型のデータにエンコードし、そのデータをデコードして元のデータに戻ることを確認するテスト"""
         data: bytes = b"test_round_trip"
         encoded_data: str = self.base64_url_handler.encode_bytes(data)
         decoded_data: bytes = self.base64_url_handler.decode_bytes(
@@ -62,10 +103,17 @@ class Base64UrlTestCase(TestCase):
         )
         self.assertEqual(decoded_data, data)
 
+    def test_encode_decode_dict_round_trip(self) -> None:
+        """Base64URLセーフ形式にdict型のデータにエンコードし、そのデータをデコードして元のデータに戻ることを確認するテスト"""
+        data: dict = {"Key": "Value"}
+        encoded_data: str = self.base64_url_handler.encode_dict(data)
+        decoded_data: dict = self.base64_url_handler.decode_dict(encoded_data)
+        self.assertEqual(decoded_data, data)
+
     def test_decode_dict_invalid_json(self) -> None:
         """
         Base64デコード後のデータが無効なJSON形式の場合のテスト。
         """
-        base64_data = "aW52YWxpZF9qc29u"
+        encoded_invalid_json_data: str = "aW52YWxpZF9qc29u"
         with self.assertRaises(ValueError):
-            self.base64_url_handler.decode_dict(base64_data)
+            self.base64_url_handler.decode_dict(encoded_invalid_json_data)
