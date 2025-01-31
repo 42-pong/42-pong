c2b4839fc48e77705f903abe20bd1ba5194800ec
taekklee <taekklee@student.42tokyo.jp>
2025-01-31 18:03:12 +0900

    :zap: (FE:GameStartPanel) ボタンが機能するようにリスナーを追加

--- frontend/pong/js/components/game/GameStartPanel.js
+++ frontend/pong/js/components/game/GameStartPanel.js
@@ -2,6 +2,8 @@ import { BootstrapDisplay } from "../../bootstrap/utilities/display";
 import { BootstrapFlex } from "../../bootstrap/utilities/flex";
 import { BootstrapSizing } from "../../bootstrap/utilities/sizing";
 import { BootstrapSpacing } from "../../bootstrap/utilities/spacing";
+import { Paths } from "../../constants/Paths";
+import { PongEvents } from "../../constants/PongEvents";
 import { Component } from "../../core/Component";
 import { createPrimaryButton } from "../../utils/elements/button/createPrimaryButton";
 import { createElement } from "../../utils/elements/createElement";
@@ -29,6 +31,11 @@ export class GameStartPanel extends Component {
     this.#menu = menu;
 
     this.#setStyle();
+
+    this._attachEventListener(
+      "click",
+      PongEvents.UPDATE_ROUTER.trigger,
+    );
   }
 
   _render() {
@@ -48,7 +55,7 @@ const getLocalMatchStartButton = () => {
 
 const getTournamentStartButton = () => {
   const tournamentStartButton = createPrimaryButton(
-    { textContent: "トーナメント開始" },
+    { textContent: "トーナメント開始", pathname: Paths.TOURNAMENTS },
     { type: "button" },
   );
   return tournamentStartButton;
