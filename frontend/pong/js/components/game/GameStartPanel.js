7bc4e3f010541646839f5177af4c96f96240b1e0
taekklee <taekklee@student.42tokyo.jp>
2025-01-31 17:18:26 +0900

    :seedling: (FE:GameStartPanel) ゲームのスタートパネルを作成

---
+++ frontend/pong/js/components/game/GameStartPanel.js
@@ -0,0 +1,55 @@
+import { BootstrapDisplay } from "../../bootstrap/utilities/display";
+import { BootstrapFlex } from "../../bootstrap/utilities/flex";
+import { BootstrapSizing } from "../../bootstrap/utilities/sizing";
+import { BootstrapSpacing } from "../../bootstrap/utilities/spacing";
+import { Component } from "../../core/Component";
+import { createPrimaryButton } from "../../utils/elements/button/createPrimaryButton";
+import { createElement } from "../../utils/elements/createElement";
+
+export class GameStartPanel extends Component {
+  #menu;
+
+  #setStyle() {
+    BootstrapDisplay.setGrid(this.#menu);
+    BootstrapSizing.setWidth50(this.#menu);
+    BootstrapSpacing.setGap(this.#menu);
+
+    BootstrapDisplay.setFlex(this);
+    BootstrapFlex.setFlexColumn(this);
+    BootstrapFlex.setJustifyContentCenter(this);
+    BootstrapFlex.setAlignItemsCenter(this);
+    BootstrapSizing.setWidth50(this);
+    BootstrapSizing.setHeight50(this);
+  }
+
+  _onConnect() {
+    const menu = createElement("div");
+    menu.appendChild(getLocalMatchStartButton());
+    menu.appendChild(getTournamentStartButton());
+    this.#menu = menu;
+
+    this.#setStyle();
+  }
+
+  _render() {
+    this.appendChild(this.#menu);
+  }
+}
+
+const getLocalMatchStartButton = () => {
+  const localMatchStartButton = createPrimaryButton(
+    { textContent: "ローカル対戦" },
+    { type: "button" },
+  );
+  // TODO: [DELETE] ローカルマッチの準備とともに削除
+  localMatchStartButton.setAttribute("disabled", "");
+  return localMatchStartButton;
+};
+
+const getTournamentStartButton = () => {
+  const tournamentStartButton = createPrimaryButton(
+    { textContent: "トーナメント開始" },
+    { type: "button" },
+  );
+  return tournamentStartButton;
+};
