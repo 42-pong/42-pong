db1555b9e9ddee763e5efcac9527ab186df00208
habvi <93956895+habvi@users.noreply.github.com>
2025-02-01 00:26:50 +0900

    :zap:(users) ユーザープロフィール一覧取得ができていることを確認するテスト追加

--- backend/pong/users/tests/integration/test_list.py
+++ backend/pong/users/tests/integration/test_list.py
@@ -2,16 +2,21 @@ from typing import Final
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from rest_framework import test
+from rest_framework import response as drf_response
+from rest_framework import status, test
 
 from accounts import constants
 from accounts.player import models
+from pong.custom_response import custom_response
 
+ID: Final[str] = constants.UserFields.ID
 USERNAME: Final[str] = constants.UserFields.USERNAME
 EMAIL: Final[str] = constants.UserFields.EMAIL
 PASSWORD: Final[str] = constants.UserFields.PASSWORD
 USER: Final[str] = constants.PlayerFields.USER
 DISPLAY_NAME: Final[str] = constants.PlayerFields.DISPLAY_NAME
+
+DATA: Final[str] = custom_response.DATA
 
 
 class UsersListViewTests(test.APITestCase):
@@ -63,3 +68,42 @@ class UsersListViewTests(test.APITestCase):
 
         self.assertTrue(User.objects.filter(id=user1.id).exists())
         self.assertTrue(User.objects.filter(id=user2.id).exists())
+
+    def test_200_no_users_exist(self) -> None:
+        """
+        ユーザーが存在しない場合、エラーにならず空のdataを返すことを確認
+        """
+        response: drf_response.Response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[DATA], {})
+
+    def test_200_get_users_list(self) -> None:
+        """
+        存在するユーザーのプロフィール一覧を取得できることを確認
+        """
+        # 2人のユーザーを作成
+        user1: User = self._create_user_and_related_player(
+            self.user_data1, self.player_data1
+        )
+        user2: User = self._create_user_and_related_player(
+            self.user_data2, self.player_data2
+        )
+        response: drf_response.Response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data[DATA],
+            [
+                {
+                    ID: user1.id,
+                    USERNAME: self.user_data1[USERNAME],
+                    DISPLAY_NAME: self.player_data1[DISPLAY_NAME],
+                },
+                {
+                    ID: user2.id,
+                    USERNAME: self.user_data2[USERNAME],
+                    DISPLAY_NAME: self.player_data2[DISPLAY_NAME],
+                },
+            ],
+        )
