431791778b45b7168b1cf80b8882a4bd6063b5af
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-01 22:53:13 +0900

    :zap: (tournaments/participations) swagger-uiにリクエスト例と正常レスポンス例を追加
    create: ranking以外のフィールドをリクエストで受け取り、rankingはnullで初期化
    patch: rankingのみリクエストで受け取り、rankingのみ更新

--- backend/pong/tournaments/participation/views.py
+++ backend/pong/tournaments/participation/views.py
@@ -1,5 +1,8 @@
 from drf_spectacular.utils import (
+    OpenApiExample,
     OpenApiParameter,
+    OpenApiRequest,
+    OpenApiResponse,
     extend_schema,
     extend_schema_view,
 )
@@ -30,19 +33,154 @@ from . import models, serializers
                 location=OpenApiParameter.QUERY,
             ),
         ],
+        request=None,
+        responses={
+            200: OpenApiResponse(
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    OpenApiExample(
+                        "Example 200 response",
+                        value={
+                            "status": "ok",
+                            "data": [
+                                {
+                                    "id": 1,
+                                    "tournament_id": 4,
+                                    "user_id": 7,
+                                    "participation_name": "player_x",
+                                    "ranking": 4,
+                                },
+                                {
+                                    "id": 2,
+                                    "tournament_id": 4,
+                                    "user_id": 7,
+                                    "participation_name": "player_y",
+                                    "ranking": None,
+                                },
+                                {"...", "..."},
+                            ],
+                        },
+                    ),
+                ],
+            ),
+        },
     ),
     retrieve=extend_schema(
         description="指定されたIDのParticipationレコードの詳細を取得する。",
         operation_id="participations_retrieve_by_id",  # 明示的にoperationIdを設定
+        responses={
+            200: OpenApiResponse(
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    OpenApiExample(
+                        "Example 200 response",
+                        value={
+                            "status": "ok",
+                            "data": {
+                                "id": 1,
+                                "tournament_id": 2,
+                                "user_id": 3,
+                                "participation_name": "player_x",
+                                "ranking": 1,
+                            },
+                        },
+                    ),
+                ],
+            ),
+        },
     ),
     create=extend_schema(
         description="新しいParticipationレコードを作成する。",
+        request=OpenApiRequest(
+            request={"type": "object"},
+            examples=[
+                OpenApiExample(
+                    "Example request",
+                    value={
+                        "tournament_id": 1,
+                        "player_id": 2,
+                        "participation_name": "player_x",
+                    },
+                ),
+            ],
+        ),
+        responses={
+            201: OpenApiResponse(
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    OpenApiExample(
+                        "Example 201 response",
+                        value={
+                            "status": "ok",
+                            "data": {
+                                "id": 1,
+                                "tournament_id": 2,
+                                "user_id": 3,
+                                "participation_name": "player_x",
+                                "ranking": None,
+                            },
+                        },
+                    ),
+                ],
+            ),
+        },
     ),
     partial_update=extend_schema(
         description="指定されたIDのParticipationレコードのランキングを登録します。",
+        request=OpenApiRequest(
+            request={"type": "object"},
+            examples=[
+                OpenApiExample(
+                    "Example request",
+                    value={
+                        "ranking": 1,
+                    },
+                ),
+            ],
+        ),
+        responses={
+            200: OpenApiResponse(
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    OpenApiExample(
+                        "Example 200 response",
+                        value={
+                            "status": "ok",
+                            "data": {
+                                "id": 1,
+                                "tournament_id": 2,
+                                "user_id": 3,
+                                "participation_name": "player_x",
+                                "ranking": 1,
+                            },
+                        },
+                    ),
+                ],
+            ),
+        },
     ),
     destroy=extend_schema(
         description="指定されたIDのParticipationレコードを削除する。",
+        responses={
+            204: OpenApiResponse(
+                response={"type": "object"},  # todo: serializerに変える
+                examples=[
+                    OpenApiExample(
+                        "Example 204 response",
+                        value={
+                            "status": "ok",
+                            "data": {
+                                "id": 1,
+                                "tournament_id": 2,
+                                "user_id": 3,
+                                "participation_name": "player_x",
+                                "ranking": 1,
+                            },
+                        },
+                    ),
+                ],
+            ),
+        },
     ),
 )
 class ParticipationViewSet(viewsets.ModelViewSet):
