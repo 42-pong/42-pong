f1709095d7c2fcf33c3621a6bb9a92fc9b019c30
taekklee <taekklee@student.42tokyo.jp>
2025-02-04 18:17:11 +0900

    :seedling: (FE:TournamentEnums) トーナメント関連 Enums を追加

---
+++ frontend/pong/js/enums/TournamentEnums.js
@@ -0,0 +1,15 @@
+const Stage = {
+  ENTRANCE: "ENTRANCE",
+  PROGRESS: "PROGRESS",
+};
+
+const Progress = {
+  WAITING: "WAITING",
+  ONGOING: "ONGOING",
+  FINISHED: "FINISHED",
+};
+
+export const TournamentEnums = Object.freeze({
+  Stage,
+  Progress,
+});
