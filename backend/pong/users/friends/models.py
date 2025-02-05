99ae80f73440b1b806f837c859bd233e8d3187d0
habvi <93956895+habvi@users.noreply.github.com>
2025-02-06 01:27:16 +0900

    :zap:(friends,model) 自分自身をフレンドに追加しようとするとmodelレベルでエラーになるように追加

--- backend/pong/users/friends/models.py
+++ backend/pong/users/friends/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from . import constants
@@ -36,3 +37,21 @@ class Friendship(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} added {self.friend.username} as a friend"
+
+    def clean(self) -> None:
+        """
+        save()される前にバリデーションを行うclean()のオーバーライド
+        """
+        # 自分自身をフレンドに追加しようとした場合はエラー
+        if self.user == self.friend:
+            raise ValidationError("You cannot add yourself as a friend.")
+        super().clean()
+
+    # args,kwargsは型ヒントが複雑かつそのままsuper()に渡したいためignoreで対処
+    def save(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        """
+        save()のオーバーライド
+        保存する前にcleanメソッドを明示的に呼び出す必要がある
+        """
+        self.full_clean()
+        super().save(*args, **kwargs)
