09eb32cfccb018624b2ae90c30664514fd0561e3
habvi <93956895+habvi@users.noreply.github.com>
2025-02-08 00:37:58 +0900

    :zap:(friends) フレンド関連のviewsetを作成し、GETメソッドの実装list()をオーバーライド
    - viewsetの場合はlist()を書かなくても動くが、カスタムレスポンスを返せないのと、他のメソッドは少し複雑でオーバーライドするので統一して書くことにしました
    - カスタムレスポンスはdispatch()に相当する全メソッドのwrapperがあれば分かり次第変更する

--- backend/pong/users/friends/views.py
+++ backend/pong/users/friends/views.py
@@ -1,5 +1,36 @@
-from rest_framework import viewsets
+from django.contrib.auth.models import AnonymousUser, User
+from django.db.models.query import QuerySet
+from rest_framework import permissions, request, response, status, viewsets
+
+from pong.custom_response import custom_response
+
+from . import models
+from .serializers import list_serializers
 
 
-class FriendsViewSet(viewsets.ViewSet):
-    pass
+# todo: 各メソッドにtry-exceptを書いて予期せぬエラー(実装上のミスを含む)の場合に500を返す
+class FriendsViewSet(viewsets.ModelViewSet):
+    queryset = models.Friendship.objects.all().select_related("user", "friend")
+    permission_classes = (permissions.IsAuthenticated,)
+
+    http_method_names = ["get"]
+
+    def list(self, request: request.Request) -> response.Response:
+        """
+        自分のフレンドのユーザープロフィール一覧を取得するGETメソッド
+        """
+        user: User | AnonymousUser = request.user
+        if isinstance(user, AnonymousUser):
+            return custom_response.CustomResponse(
+                errors={"user": "The user does not exist."},
+                status=status.HTTP_404_NOT_FOUND,  # todo: 404ではないかも
+            )
+
+        # 自分のフレンド一覧を取得
+        friends: QuerySet[models.Friendship] = self.queryset.filter(user=user)
+        list_serializer: list_serializers.FriendshipListSerializer = (
+            list_serializers.FriendshipListSerializer(friends, many=True)
+        )
+        return custom_response.CustomResponse(
+            data=list_serializer.data, status=status.HTTP_200_OK
+        )
