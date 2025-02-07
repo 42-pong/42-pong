fc5851faa846f2dbfb2a4cd5268ab2ba8ce51218
habvi <93956895+habvi@users.noreply.github.com>
2025-02-08 00:46:56 +0900

    :zap:(friends) GETメソッドの200,401,500のextend_schemaを追加

--- backend/pong/users/friends/views.py
+++ backend/pong/users/friends/views.py
@@ -1,13 +1,58 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.db.models.query import QuerySet
+from drf_spectacular import utils
 from rest_framework import permissions, request, response, status, viewsets
 
 from pong.custom_response import custom_response
 
-from . import models
+from . import constants, models
 from .serializers import list_serializers
 
 
+@utils.extend_schema_view(
+    list=utils.extend_schema(
+        responses={
+            200: utils.OpenApiResponse(
+                description="A list of friends for the authenticated user.",
+                response=list_serializers.FriendshipListSerializer(many=True),
+                examples=[
+                    utils.OpenApiExample(
+                        "Example 200 response",
+                        value={
+                            custom_response.STATUS: custom_response.Status.OK,
+                            custom_response.DATA: [
+                                {
+                                    constants.FriendshipFields.USER_ID: 1,
+                                    constants.FriendshipFields.FRIEND_USER_ID: 2,
+                                },
+                                {"...", "..."},
+                            ],
+                        },
+                    ),
+                ],
+            ),
+            # todo: 現在Djangoが自動で返している。CustomResponseが使えたら併せて変更する
+            401: utils.OpenApiResponse(
+                description="Not authenticated",
+                response={
+                    "type": "object",
+                    "properties": {"detail": {"type": "string"}},
+                },
+                examples=[
+                    utils.OpenApiExample(
+                        "Example 401 response",
+                        value={
+                            "detail": "Authentication credentials were not provided."
+                        },
+                    ),
+                ],
+            ),
+            # todo: 404は確定したら追加する
+            # todo: 詳細のschemaが必要であれば追加する
+            500: utils.OpenApiResponse(description="Internal server error"),
+        },
+    ),
+)
 # todo: 各メソッドにtry-exceptを書いて予期せぬエラー(実装上のミスを含む)の場合に500を返す
 class FriendsViewSet(viewsets.ModelViewSet):
     queryset = models.Friendship.objects.all().select_related("user", "friend")
