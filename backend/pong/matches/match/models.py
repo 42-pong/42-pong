99a8c09e40c899c9a371eb29eebdc90bc466bab8
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-08 17:01:54 +0900

    :zap: (matches) モデルを作成

--- backend/pong/matches/match/models.py
+++ backend/pong/matches/match/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from tournaments.round import models as round_models
+
+
+class Match(models.Model):
+    """
+    試合（Match）を表すモデル。
+    各試合は特定のラウンド（Round）に属する。
+
+    Attributes:
+        round : 関連するラウンドへの外部キー。
+        created_at : 試合が作成された日時。
+    """
+
+    round = models.ForeignKey(
+        round_models.Round,
+        on_delete=models.CASCADE,
+        related_name="matches",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "matches"
+
+    def __str__(self) -> str:
+        return f"Match {self.id} - Round {self.round.round_number}"
--- backend/pong/matches/participation/models.py
+++ backend/pong/matches/participation/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+import accounts.player.models as player_models
+from matches.match import models as match_models
+
+from .. import constants
+
+
+class Participation(models.Model):
+    """
+    試合の参加者（MatchParticipation）を表すモデル。
+    各試合におけるプレイヤーの情報を管理する。
+
+    Attributes:
+        match : 参加している試合への外部キー。
+        player : 参加するプレイヤーへの外部キー。
+        team : プレイヤーが所属するチーム（"1", "2"）。
+        created_at : 参加情報が作成された日時。
+    """
+
+    match = models.ForeignKey(
+        match_models.Match,
+        on_delete=models.CASCADE,
+        related_name="match_participations",
+    )
+    player = models.ForeignKey(
+        player_models.Player,
+        on_delete=models.CASCADE,
+        related_name="match_participations",
+    )
+    team = models.CharField(
+        max_length=15,
+        choices=[
+            (status.value, status.name)
+            for status in constants.ParticipationFields.TeamEnum
+        ],
+        default=constants.ParticipationFields.TeamEnum.ONE.value,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "match_participations"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["match_id", "player_id"],
+                name="unique_match_participation",
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"Player={self.player.display_name} in match_id={self.match.id}, team=({self.team})"
