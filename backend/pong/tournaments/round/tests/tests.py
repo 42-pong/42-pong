1bffb05f0663becb6daf2d76cb28c04a8aae8a27
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-08 22:22:14 +0900

    :zap: (tournaments/round) roundモデルの単体テストを追加

---
+++ backend/pong/tournaments/round/tests/tests.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+
+from tournaments.constants import RoundFields
+from tournaments.tournament.models import Tournament
+
+from ..models import Round
+
+
+class RoundModelTest(TestCase):
+    def setUp(self) -> None:
+        # テスト用のトーナメントデータを作成
+        self.tournament = Tournament.objects.create()
+        # Player作成
+        self.round: Round = Round.objects.create(
+            tournament=self.tournament,
+            round_number=1,
+        )
+
+    def test_create_round(self) -> None:
+        """
+        ラウンドが正常に作成されるかテスト
+        """
+        # 作成したラウンドがデータベースに正しく保存されていることを確認
+        self.assertEqual(self.round.tournament, self.tournament)
+        self.assertEqual(self.round.round_number, 1)
+        self.assertEqual(
+            self.round.status, RoundFields.StatusEnum.IN_PROGRESS.value
+        )
+
+    def test_change_round_status(self) -> None:
+        """
+        ラウンドのステータスを更新できることを確認
+        """
+        # ラウンドを更新
+        self.round.status = RoundFields.StatusEnum.COMPLETED.value
+        self.round.save()
+
+        self.assertEqual(
+            self.round.status, RoundFields.StatusEnum.COMPLETED.value
+        )
+
+    def test_delete_round(self) -> None:
+        """
+        Roundを削除したら、紐づくTournamentは削除されないことを確認
+        """
+        tournament_id = self.tournament.id
+        self.round.delete()
+
+        self.assertEqual(Round.objects.count(), 0)
+        self.assertTrue(Tournament.objects.filter(id=tournament_id).exists())
+
+    def test_delete_related_tournament(self) -> None:
+        """
+        Tournamentを削除したら、紐づくRoundも削除されることを確認
+        """
+        self.tournament.delete()
+
+        self.assertEqual(Tournament.objects.count(), 0)
+        self.assertEqual(Round.objects.count(), 0)
