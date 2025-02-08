23c67ed89d0b4f935e10578a8c27bbe88e70febe
habvi <93956895+habvi@users.noreply.github.com>
2025-02-09 05:27:40 +0900

    :seedling:(avatar,identicon) usernameをhash値にて偶奇から0/1を求め,5×5マスを左右対称に埋める
    (pongと直接関係ないので興味あれば見るくらいで全然大丈夫です)

---
+++ backend/pong/accounts/player/identicon.py
@@ -0,0 +1,25 @@
+import hashlib
+
+# 正方形の一辺の長さ(マス)
+SIDE_LENGTH: int = 5
+
+
+def generate_identicon(username: str) -> None:
+
+    def _create_bit_pattern(hash_value: str) -> list[list[int]]:
+        length: int = len(hash_value)
+
+        # 16進数のhash値を10進数に変換して2で割った余りのlistを作成(0 or 1)
+        hash_bit_list: list[int] = [
+            int(hash_value[i], 16) % 2 for i in range(length)
+        ]
+        # 5x3の二次元配列に変換
+        width: int = SIDE_LENGTH // 2 + 1
+        bit_pattern: list[list[int]] = [
+            hash_bit_list[i : i + width] for i in range(0, length, width)
+        ]
+        # 線対象に折り返して5x5の二次元配列に変換
+        return [row + row[::-1][1:] for row in bit_pattern]
+
+    hash_value: str = hashlib.md5(username.encode()).hexdigest()
+    bit_pattern: list[list[int]] = _create_bit_pattern(hash_value[:15])
