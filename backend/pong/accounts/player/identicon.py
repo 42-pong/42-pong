6bd808bcfcda78a81a9e8643ed2a438bb6c94495
habvi <93956895+habvi@users.noreply.github.com>
2025-02-09 05:31:12 +0900

    :zap:(avatar,identicon) hash値から色を求める。HSLからRGBに変換
    (pongと直接関係ないので興味あれば見るくらいで全然大丈夫です)

--- backend/pong/accounts/player/identicon.py
+++ backend/pong/accounts/player/identicon.py
@@ -1,3 +1,4 @@
+import colorsys
 import hashlib
 
 # 正方形の一辺の長さ(マス)
@@ -21,5 +22,17 @@ def generate_identicon(username: str) -> None:
         # 線対象に折り返して5x5の二次元配列に変換
         return [row + row[::-1][1:] for row in bit_pattern]
 
+    def _create_color(hash_value: str) -> tuple[int, int, int]:
+        # 色相
+        hue: float = int(hash_value[0:3], 16) / 4095 * 360
+        # 彩度
+        saturation: float = 0.65 - int(hash_value[3:5], 16) / 255 * 0.2
+        # 輝度
+        luminance: float = 0.75 - int(hash_value[5:7], 16) / 255 * 0.2
+        # HSLからRGBに変換
+        r, g, b = colorsys.hls_to_rgb(hue / 360, luminance, saturation)
+        return (int(r * 255), int(g * 255), int(b * 255))
+
     hash_value: str = hashlib.md5(username.encode()).hexdigest()
     bit_pattern: list[list[int]] = _create_bit_pattern(hash_value[:15])
+    color: tuple[int, int, int] = _create_color(hash_value[-7:])
