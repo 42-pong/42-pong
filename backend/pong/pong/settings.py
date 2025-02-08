265421e17563ac6087f0f7cc29c47144682b4f1c
habvi <93956895+habvi@users.noreply.github.com>
2025-02-09 03:44:43 +0900

    :zap:(avatar,settings) アバター画像(media)を使用するための設定追加

--- backend/pong/pong/settings.py
+++ backend/pong/pong/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # todo mypyが型対応していない3rdパーティのライブラリも型対応できるようにする
@@ -186,6 +187,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
