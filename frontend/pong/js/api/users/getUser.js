726517bb906eeee9e18dbff8376871770e92e6a8
taekklee <taekklee@student.42tokyo.jp>
2025-02-09 13:13:00 +0900

    :zap: (FE:getUser) 無効な userId は fetch 前に処理できるように変更

--- frontend/pong/js/api/users/getUser.js
+++ frontend/pong/js/api/users/getUser.js
@@ -3,6 +3,13 @@ import { fetchData } from "../utils/fetchData";
 import { convertUserData } from "./convertUserData";
 
 export async function getUser(userId) {
+  if (!isValid(userId)) {
+    return {
+      user: null,
+      error: new Error("userId: not a 'string' type or empty"),
+    };
+  }
+
   const { data, error } = await fetchData(
     Endpoints.USERS.withId(userId).href,
   );
@@ -10,3 +17,6 @@ export async function getUser(userId) {
   const user = error ? null : convertUserData(data);
   return { user, error };
 }
+
+const isValid = (userId) =>
+  typeof userId === "string" && userId !== "";
