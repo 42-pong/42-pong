70a97743c2f9786b5f95daa4eb66418dbbe1c9ea
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-10 14:40:00 +0900

    :zap: (matches/match) model単体テストのテストケース作成

--- backend/pong/matches/match/tests/test_models.py
+++ backend/pong/matches/match/tests/test_models.py
@@ -24,3 +24,32 @@ class MatchModelTest(TestCase):
             tournament=self.tournament, round_number=1
         )
         self.match = Match.objects.create(round=self.round)
+
+    def test_match_creation(self) -> None:
+        """
+        Matchが正常に作成されるかテスト
+        """
+        # DBの値を反映
+        self.round.refresh_from_db()
+        self.match.refresh_from_db()
+
+        self.assertEqual(self.match.round, self.round)
+
+    def test_delete_match(self) -> None:
+        """
+        Matchを削除し、紐づくRoundは削除されないことを確認
+        """
+        round_id = self.round.id
+        self.match.delete()
+
+        self.assertFalse(Match.objects.exists())
+        self.assertTrue(Round.objects.filter(id=round_id).exists())
+
+    def test_delete_round_deletes_match(self) -> None:
+        """
+        Roundを削除し、紐づくMatchも削除されることを確認
+        """
+        self.round.delete()
+
+        self.assertFalse(Round.objects.exists())
+        self.assertFalse(Match.objects.exists())
