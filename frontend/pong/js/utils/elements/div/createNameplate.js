28fb6e7f73f0ecd37e5ff3b0e33cefbb01b42b9f
taekklee <taekklee@student.42tokyo.jp>
2025-02-13 12:32:52 +0900

    :zap: (FE:nameplate) アバターイメージの高さを設定可能に変更

--- frontend/pong/js/utils/elements/div/createNameplate.js
+++ frontend/pong/js/utils/elements/div/createNameplate.js
@@ -3,12 +3,14 @@ import { BootstrapFlex } from "../../../bootstrap/utilities/flex";
 import { BootstrapSpacing } from "../../../bootstrap/utilities/spacing";
 import { BootstrapText } from "../../../bootstrap/utilities/text";
 import { createElement } from "../createElement";
+import { setHeight } from "../style/setHeight";
 
-export const createNameplate = (user) => {
+export const createNameplate = (user, avatarHeight = "") => {
   // TODO: 実際のアバターを使用
   const avatar = new Image();
   avatar.src = "https://placehold.co/30";
   avatar.alt = "sample image";
+  if (avatarHeight) setHeight(avatar, avatarHeight);
 
   const nameTag = createElement("span", {
     textContent: `${user.displayName}#${user.username}`,
