ec9b430c201b9e5e0d70feff11261e9dc3c2e4a0
habvi <93956895+habvi@users.noreply.github.com>
2025-02-15 17:27:11 +0900

    :zap:(accounts) superuserで使われているpasswordのvalidatorを一般userにも適用

--- backend/pong/accounts/user/serializers.py
+++ backend/pong/accounts/user/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers, validators
 
 from .. import constants
@@ -20,6 +21,13 @@ class UserSerializer(serializers.ModelSerializer):
             validators.UniqueValidator(queryset=User.objects.all()),
         ]
     )
+    # validate_password: 4つのvalidatorでチェックされる
+    # https://docs.djangoproject.com/ja/5.1/topics/auth/passwords/#included-validators
+    password = serializers.CharField(
+        write_only=True,
+        max_length=50,
+        validators=[validate_password],
+    )
 
     class Meta:
         model = User
@@ -29,9 +37,6 @@ class UserSerializer(serializers.ModelSerializer):
             constants.UserFields.EMAIL,
             constants.UserFields.PASSWORD,
         )
-        extra_kwargs = {
-            constants.UserFields.PASSWORD: {"write_only": True},
-        }
 
     # PlayerSerializerのuser_serializer.save()の内部で呼ばれる
     def create(self, validated_data: dict) -> User:
