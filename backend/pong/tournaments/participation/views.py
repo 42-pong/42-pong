55092bb0c8ab47f8e299f7970b1f7f9b47a088e7
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-15 17:41:09 +0900

    :zap: (tournaments/participation) user-idでフィルタリングできる機能を追加

--- backend/pong/tournaments/participation/views.py
+++ backend/pong/tournaments/participation/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from drf_spectacular.utils import (
     OpenApiExample,
     OpenApiParameter,
@@ -6,6 +7,8 @@ from drf_spectacular.utils import (
     extend_schema_view,
 )
 from rest_framework import viewsets
+
+from accounts.player import models as player_models
 
 from . import models, serializers
 
@@ -44,21 +47,21 @@ from . import models, serializers
                             "data": [
                                 {
                                     "id": 1,
+                                    "tournament_id": 4,
+                                    "user_id": 7,
                                     "participation_name": "player_x",
                                     "ranking": 4,
                                     "created_at": "2025-01-01T00:00:00.000000+09:00",
                                     "updated_at": "2025-01-01T00:30:00.000000+09:00",
-                                    "tournament_id": 4,
-                                    "user_id": 7,
                                 },
                                 {
                                     "id": 2,
+                                    "tournament_id": 5,
+                                    "user_id": 7,
                                     "participation_name": "player_y",
                                     "ranking": None,
                                     "created_at": "2025-01-01T00:01:00.000000+09:00",
                                     "updated_at": "2025-01-01T00:01:00.000000+09:00",
-                                    "tournament_id": 5,
-                                    "user_id": 7,
                                 },
                                 {"...", "..."},
                             ],
@@ -96,5 +99,26 @@ from . import models, serializers
     ),
 )
 class ParticipationReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = models.Participation.objects.all()
     serializer_class = serializers.ParticipationSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = models.Participation.objects.all()
+        user_id = self.request.query_params.get("user-id")
+        tournament_id = self.request.query_params.get("tournament-id")
+
+        if user_id:
+            # 例外を出すと面倒なのでfilterとfirstを使っている
+            player = player_models.Player.objects.filter(
+                user_id=user_id
+            ).first()
+            if player:
+                queryset = queryset.filter(player_id=player.id)
+            else:
+                queryset = (
+                    queryset.none()
+                )  # プレイヤーが見つからなければ空のクエリセットを返す
+
+        if tournament_id:
+            queryset = queryset.filter(tournament_id=tournament_id)
+
+        return queryset
