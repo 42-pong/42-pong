41cae0ad6b115656fc0c99737b4f828bb936e0d8
taekklee <taekklee@student.42tokyo.jp>
2025-02-16 22:20:45 +0900

    :seedling: (FE:DataManager) 通知機能のあるデータ管理者を作成

---
+++ frontend/pong/js/session/DataManager.js
@@ -0,0 +1,43 @@
+export class DataManager {
+  #data;
+  #observers;
+
+  constructor(data = {}) {
+    this.#data = data;
+    this.#observers = new Set();
+  }
+
+  init(data = {}) {
+    this.#data = data;
+    this.#notifyAll();
+  }
+
+  updateData(newData) {
+    if (!newData) return;
+    Object.assign(this.#data, newData);
+    this.#notifyAll();
+  }
+
+  attach(observer) {
+    const isAttachable = (observer) =>
+      observer instanceof Function && !this.#observers.has(observer);
+    if (!isAttachable(observer)) return;
+
+    this.#observers.add(observer);
+  }
+
+  detach(observer) {
+    if (!this.#observers.has(observer)) return;
+    this.#observers.delete(observer);
+  }
+
+  #notify(observer) {
+    observer(this.#data);
+  }
+
+  #notifyAll() {
+    for (const observer of this.#observers) {
+      this.#notify(observer);
+    }
+  }
+}
