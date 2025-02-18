5ecb781aabf9f63d6c614a24f458bc8a45bcc0b7
habvi <93956895+habvi@users.noreply.github.com>
2025-02-19 05:04:51 +0900

    :zap:(blocks,test) 正しいブロック関係が作れているかと,on_delete=CASCADEが正しく動作しているかのテスト追加

--- backend/pong/users/blocks/tests/test_block_relationship_model.py
+++ backend/pong/users/blocks/tests/test_block_relationship_model.py
@@ -56,3 +56,76 @@ class BlockRelationshipModelTestCase(TestCase):
                 id=self.block_relationship.id
             ).exists()
         )
+
+    def test_valid_related_user(self) -> None:
+        """
+        作成したBlockRelationshipが正しく2人のuserに紐づいていることを確認
+        """
+        user_id: int = self.block_relationship.user.id
+        user: User = User.objects.get(id=user_id)
+        # BlockRelationshipから取得したuserがuser1と一致することを確認
+        self.assertEqual(user.username, self.user_data1[USERNAME])
+
+        blocked_user_id: int = self.block_relationship.blocked_user.id
+        blocked_user: User = User.objects.get(id=blocked_user_id)
+        # BlockRelationshipから取得したblocked_userがuser2と一致することを確認
+        self.assertEqual(blocked_user.username, self.user_data2[USERNAME])
+
+    def test_delete_block_relationship(self) -> None:
+        """
+        BlockRelationshipを削除しても、userは削除されないことを確認
+        """
+        block_relationship_id: int = self.block_relationship.id
+        # BlockRelationship削除
+        self.block_relationship.delete()
+
+        # BlockRelationshipが削除されていることを確認
+        self.assertFalse(
+            models.BlockRelationship.objects.filter(
+                id=block_relationship_id
+            ).exists()
+        )
+        # userが2人とも削除されていないことを確認
+        self.assertTrue(User.objects.filter(id=self.user1.id).exists())
+        self.assertTrue(User.objects.filter(id=self.user2.id).exists())
+
+    def test_delete_related_user(self) -> None:
+        """
+        userを削除すると、BlockRelationshipも削除されることを確認
+        """
+        user_id: int = self.user1.id
+        blocked_user_id: int = self.user2.id
+        block_relationship_id: int = self.block_relationship.id
+        # user1を削除
+        self.user1.delete()
+
+        # BlockRelationshipが削除されていることを確認
+        self.assertFalse(
+            models.BlockRelationship.objects.filter(
+                id=block_relationship_id
+            ).exists()
+        )
+        # user1は削除され、user2は削除されていないことを確認
+        self.assertFalse(User.objects.filter(id=user_id).exists())
+        self.assertTrue(User.objects.filter(id=blocked_user_id).exists())
+
+    def test_delete_related_blocked_user(self) -> None:
+        """
+        blocked_userを削除すると、BlockRelationshipも削除されることを確認
+        """
+        user_id: int = self.user1.id
+        blocked_user_id: int = self.user2.id
+        block_relationship_id: int = self.block_relationship.id
+        # user2を削除
+        self.user2.delete()
+
+        # BlockRelationshipが削除されていることを確認
+        self.assertFalse(
+            models.BlockRelationship.objects.filter(
+                id=block_relationship_id
+            ).exists()
+        )
+        # user1は削除されておらず、user2は削除されていることを確認
+        self.assertTrue(User.objects.filter(id=user_id).exists())
+        self.assertFalse(User.objects.filter(id=blocked_user_id).exists())
+
