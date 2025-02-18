4a6c548a7f6dc496580c3fee50ad1dc069e1c53e
habvi <93956895+habvi@users.noreply.github.com>
2025-02-19 04:44:43 +0900

    :zap:(blocks) BlockRelationshipテーブルをadminサイトで表示できるように

--- backend/pong/users/blocks/constants.py
+++ backend/pong/users/blocks/constants.py
@@ -3,5 +3,8 @@ import dataclasses
 
 @dataclasses.dataclass(frozen=True)
 class BlockRelationshipFields:
+    ID: str = "id"
     USER_ID: str = "user_id"
     BLOCKED_USER_ID: str = "blocked_user_id"
+    CREATED_AT: str = "created_at"
+    UPDATED_AT: str = "updated_at"
