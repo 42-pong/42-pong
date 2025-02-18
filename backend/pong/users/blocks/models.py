3b271171fe547f212777fae5c55b146455ad6bdd
habvi <93956895+habvi@users.noreply.github.com>
2025-02-19 04:49:07 +0900

    :zap:(blocks,model) 自分自身をブロックしようとするとmodelレベルでエラーになるように追加

--- backend/pong/users/blocks/models.py
+++ backend/pong/users/blocks/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from . import constants
@@ -37,3 +38,21 @@ class BlockRelationship(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} block {self.blocked_user.username}"
+
+    def clean(self) -> None:
+        """
+        save()される前にバリデーションを行うclean()のオーバーライド
+        """
+        # 自分自身をブロックしようとした場合はエラー
+        if self.user == self.blocked_user:
+            raise ValidationError("You cannot block yourself.")
+        super().clean()
+
+    # args,kwargsは型ヒントが複雑かつそのままsuper()に渡したいためignoreで対処
+    def save(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
+        """
+        save()のオーバーライド
+        保存する前にcleanメソッドを明示的に呼び出す必要がある
+        """
+        self.full_clean()
+        super().save(*args, **kwargs)
