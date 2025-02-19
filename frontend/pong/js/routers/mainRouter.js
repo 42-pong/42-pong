37cd42642734c4d518a9b89b2f7be2a2c78db596
taekklee <taekklee@student.42tokyo.jp>
2025-02-19 16:03:46 +0900

    :seedling: (FE:LoadingView,ErrorView) ローディングやエラーの対応追加

--- frontend/pong/js/routers/mainRouter.js
+++ frontend/pong/js/routers/mainRouter.js
@@ -1,5 +1,7 @@
+import { ErrorView } from "../components/views/ErrorView";
 import { FriendsView } from "../components/views/FriendsView";
 import { HomeView } from "../components/views/HomeView";
+import { LoadingView } from "../components/views/LoadingView";
 import { MainView } from "../components/views/MainView";
 import { MyPageView } from "../components/views/MyPageView";
 import { NotFoundView } from "../components/views/NotFoundView";
@@ -16,6 +18,8 @@ export const mainRouter = (target) => {
     [MainView.Paths.MYPAGE]: Route.defaultRoute(MyPageView),
     [MainView.Paths.TOURNAMENTS]: Route.defaultRoute(TournamentsView),
     [MainView.Paths.NOT_FOUND]: Route.defaultRoute(NotFoundView),
+    [MainView.Paths.LOADING]: Route.defaultRoute(LoadingView),
+    [MainView.Paths.ERROR]: Route.defaultRoute(ErrorView),
   };
   const defaultRoute = Route.defaultRoute(NotFoundView);
   return new Router(target, defaultRoute, routes);
