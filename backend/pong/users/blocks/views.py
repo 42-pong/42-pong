c12ec892ecd808c1f4d62d9990449b145bc61ebc
habvi <93956895+habvi@users.noreply.github.com>
2025-02-19 23:10:13 +0900

    :zap:(blocks,views) ブロック用viewsetsの土台追加

--- backend/pong/users/blocks/views.py
+++ backend/pong/users/blocks/views.py
@@ -1,5 +1,46 @@
-from rest_framework import viewsets
+import logging
+
+from django.db.models import Q
+from rest_framework import (
+    exceptions,
+    permissions,
+    response,
+    status,
+    viewsets,
+)
+
+from pong.custom_response import custom_response
+from users import constants as users_constants
+
+from . import models
+
+logger = logging.getLogger(__name__)
 
 
 class BlocksViewSet(viewsets.ViewSet):
-    pass
+    queryset = models.BlockRelationship.objects.filter(
+        Q(blocked_user__player__isnull=False)
+    ).select_related("user", "blocked_user")
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def handle_exception(self, exc: Exception) -> response.Response:
+        """
+        ModelViewSetのhandle_exception()をオーバーライド
+        viewでtry-exceptしていない例外をカスタムレスポンスに変換して返す
+        """
+        if isinstance(
+            exc, (exceptions.NotAuthenticated, exceptions.AuthenticationFailed)
+        ):
+            logger.error(f"[401] Authentication error: {str(exc)}")
+            # 401はCustomResponseにせずそのまま返す
+            return super().handle_exception(exc)
+
+        logger.error(f"[500] Internal server error: {str(exc)}")
+        response: custom_response.CustomResponse = (
+            custom_response.CustomResponse(
+                code=[users_constants.Code.INTERNAL_ERROR],
+                errors={"detail": str(exc)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        )
+        return response
