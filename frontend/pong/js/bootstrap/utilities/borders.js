4a24ae6bacf52b0886ce5d167f54771c25a4b400
taekklee <taekklee@student.42tokyo.jp>
2025-02-19 16:12:47 +0900

    :zap: (FE:Bootstrap) borders:setDanger を一つ追加

--- frontend/pong/js/bootstrap/utilities/borders.js
+++ frontend/pong/js/bootstrap/utilities/borders.js
@@ -8,7 +8,12 @@ const setRoundedCircle = (element) => {
   return setClassNames(element, "rounded-circle");
 };
 
+const setDanger = (element) => {
+  return setClassNames(element, "border-danger");
+};
+
 export const BootstrapBorders = {
   setRounded,
   setRoundedCircle,
+  setDanger,
 };
