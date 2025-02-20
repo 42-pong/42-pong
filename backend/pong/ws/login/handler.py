dd3f7319ad68aa6c718f733cae91ce69a808d168
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-21 00:24:41 +0900

    :zap: (ws/login) 未完成のフォロワーに通知する関数を追加
    - 通知するためのメッセージルールが決まったら更新する

--- backend/pong/ws/login/handler.py
+++ backend/pong/ws/login/handler.py
@@ -4,6 +4,7 @@ from channels.layers import BaseChannelLayer  # type: ignore
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from users.friends import models as friend_models
 from ws.share.async_redis_client import AsyncRedisClient  # type: ignore
 
 from ..share import channel_handler
@@ -31,8 +32,20 @@ class LoginHandler:
     async def handle(self, payload: dict) -> None:
         """
         クライアントからの入力を受け取り、正しい入力であればログインとして扱うためのハンドラ関数
+
+        Exceptions:
+            ValidationError: user_idが有効でなかった場合
+            KeyError: user_idがint出なかった場合
         """
+        # すでにログイン済みであれば無視
+        if self.user_id:
+            return
+
         self.user_id = payload[login_constants.USER_ID]
+
+        # バリデーション失敗したら例外を投げる
+        await self._validate_user_id()
+        await self._login()
 
     async def _login(self) -> None:
         """
@@ -44,6 +57,8 @@ class LoginHandler:
             login_constants.CHANNEL_RESOURCE,
             self.channel_handler.channel_name,
         )
+        await self._send_login_result(login_constants.Status.OK.value)
+        # TODO: followerに通知する処理をmessageルールが決まったら追加
 
     async def logout(self) -> None:
         """
@@ -56,6 +71,7 @@ class LoginHandler:
             login_constants.CHANNEL_RESOURCE,
             self.channel_handler.channel_name,
         )
+        # TODO: followerに通知する処理をmessageルールが決まったら追加
 
     async def _validate_user_id(self) -> None:
         """
@@ -82,3 +98,27 @@ class LoginHandler:
         await self.channel_handler.send_to_consumer(
             message, self.channel_handler.channel_name
         )
+
+    # TODO: ログインかログアウトかを引数で受け取る
+    async def _notify_followers(self) -> None:
+        # TODO: 決定したmessageに変更
+        message = {
+            ws_constants.Category.key(): ws_constants.Category.LOGIN,
+            ws_constants.PAYLOAD_KEY: {login_constants.USER_ID: self.user_id},
+        }
+        # オンライン状態でフレンド登録している人をイテレーションで順に処理
+        async for follower_id in (
+            friend_models.Friendship.objects.filter(friend_id=self.user_id)
+            .values_list("id", flat=True)
+            .aiterator()
+        ):
+            # followerのうちがオンライン状態であるユーザーのchannel名の集合を取得
+            channel_set: set[str] = await AsyncRedisClient.smembers_value(
+                login_constants.USER_NAMESPACE,
+                follower_id,
+                login_constants.CHANNEL_RESOURCE,
+            )
+
+            # onlineのフォロワーのchannelすべてに送信
+            for channel in channel_set:
+                await self.channel_handler.send_to_consumer(message, channel)
