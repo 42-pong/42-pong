08eb239d7f67bbc0276f4db30e34b6916c6b8082
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-20 23:53:32 +0900

    :zap: (ws/login) redisにonlineユーザーのchannel名を保存するためのキーの定数を定義

--- backend/pong/ws/login/constants.py
+++ backend/pong/ws/login/constants.py
@@ -2,6 +2,9 @@ from typing import Final
 
 from ..share import constants as ws_constants
 
+USER_NAMESPACE: Final[str] = "user"
+CHANNEL_RESOURCE: Final[str] = "channels"
+
 USER_ID: Final[str] = "user_id"
 
 
