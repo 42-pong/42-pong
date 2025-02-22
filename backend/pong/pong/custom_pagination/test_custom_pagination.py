092d0e6c7594e46378d8ea6055de14362d41b824
habvi <93956895+habvi@users.noreply.github.com>
2025-02-23 04:28:07 +0900

    :zap:(pagination,test) page sizeがアイテム数より少ない場合に複数ページに分かれて全アイテムが返ることを確認

--- backend/pong/pong/custom_pagination/test_custom_pagination.py
+++ backend/pong/pong/custom_pagination/test_custom_pagination.py
@@ -56,3 +56,58 @@ class CustomPaginationTests(TestCase):
                 RESULTS: self.paginated_data,  # 3アイテム
             },
         )
+
+    def test_get_200_low_page_size(self) -> None:
+        """
+        page sizeよりアイテム数が多い場合、複数ページに分かれて全てのアイテムが返ることを確認
+        page sizeを2に設定し、1ページ目は2アイテム・2ページ目は1アイテムが返る
+        """
+        paginator: custom_pagination.CustomPagination = (
+            custom_pagination.CustomPagination(page_size=2)  # page_sizeを指定
+        )
+
+        # 1ページ目を取得
+        request: drf_request.Request = drf_request.Request(
+            self.factory.get("/")
+        )
+        paginated_data: Optional[list] = paginator.paginate_queryset(
+            self.paginated_data,  # type: ignore[arg-type]
+            request,
+        )
+        response: drf_response.Response = paginator.get_paginated_response(
+            paginated_data  # type: ignore[arg-type]
+        )
+        # 1ページ目のdataを確認
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data[DATA],
+            {
+                COUNT: 3,
+                NEXT: "http://testserver/?page=2",
+                PREVIOUS: None,
+                RESULTS: [self.data1, self.data2],  # 2アイテム
+            },
+        )
+
+        # 2ページ目を取得
+        request2: drf_request.Request = drf_request.Request(
+            self.factory.get("/?page=2")
+        )
+        paginated_data2: Optional[list] = paginator.paginate_queryset(
+            self.paginated_data,  # type: ignore[arg-type]
+            request2,
+        )
+        response2: drf_response.Response = paginator.get_paginated_response(
+            paginated_data2  # type: ignore[arg-type]
+        )
+        # 2ページ目のdataを確認
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response2.data[DATA],
+            {
+                COUNT: 3,
+                NEXT: None,
+                PREVIOUS: "http://testserver/",
+                RESULTS: [self.data3],  # 1アイテム
+            },
+        )
