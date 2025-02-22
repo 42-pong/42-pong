b5f208ae40a2e9aa29eb0a6fbe1a571673db88c7
taekklee <taekklee@student.42tokyo.jp>
2025-02-22 19:10:05 +0900

    :recycle: (FE:SignInButton) 仮のものからログインページへの移動に変更

--- frontend/pong/js/components/auth/SignInButton.js
+++ frontend/pong/js/components/auth/SignInButton.js
@@ -1,5 +1,6 @@
+import { Paths } from "../../constants/Paths";
+import { PongEvents } from "../../constants/PongEvents";
 import { StyledButton } from "../../core/StyledButton";
-import { UserSessionManager } from "../../session/UserSessionManager";
 
 export class SignInButton extends StyledButton {
   constructor(state = {}, attributes = {}) {
@@ -17,17 +18,10 @@ export class SignInButton extends StyledButton {
   _onConnect() {
     this._attachEventListener("click", (event) => {
       event.preventDefault();
-      event.stopPropagation();
 
-      // TODO: DELETE: 一時的な対処なので確認が必要
-      const mockUser = {
-        id: 1,
-        username: "user",
-        email: "username1@example.com",
-        displayName: "mock",
-        avatar: "https://placehold.co/30",
-      };
-      UserSessionManager.signIn(mockUser, {});
+      this.dispatchEvent(
+        PongEvents.UPDATE_ROUTER.create(Paths.LOGIN),
+      );
     });
   }
 }
