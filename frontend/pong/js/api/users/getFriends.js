228480f6d7fdcb69d1b3410493fb6364f41afb94
taekklee <taekklee@student.42tokyo.jp>
2025-02-22 19:06:47 +0900

    :zap: (FE:api) 認証つきの api に修正

--- frontend/pong/js/api/users/getFriends.js
+++ frontend/pong/js/api/users/getFriends.js
@@ -1,10 +1,12 @@
 import { Endpoints } from "../../constants/Endpoints";
-import { fetchData } from "../utils/fetchData";
+import { fetchAuthenticatedData } from "../utils/fetchAuthenticatedData";
 import { convertFriendData } from "./convertFriendData";
 
 export async function getFriends() {
   // TODO: 認証つきのものに変更
-  const { data, error } = await fetchData(Endpoints.FRIENDS.href);
+  const { data, error } = await fetchAuthenticatedData(
+    Endpoints.FRIENDS.href,
+  );
 
   const users = error ? [] : data.map(convertFriendData);
   return { users, error };
---
+++ frontend/pong/js/api/utils/fetchAuthenticatedData.js
@@ -0,0 +1,58 @@
+import { Endpoints } from "../../constants/Endpoints";
+import { UserSessionManager } from "../../session/UserSessionManager";
+import { fetchData } from "./fetchData";
+
+export const fetchAuthenticatedData = async (
+  url,
+  options = {},
+  isRetry = false,
+) => {
+  const result = {
+    data: null,
+    error: null,
+  };
+
+  const accessToken =
+    UserSessionManager.getInstance().getAccessToken();
+
+  try {
+    const headers = new Headers(options.headers ?? {});
+    headers.append("Authorization", `Bearer ${accessToken}`);
+
+    const res = await fetch(url, { ...options, headers });
+
+    if (res.status === 401 && !isRetry)
+      return retryWithRefreshToken(url, options);
+
+    const { status, data } = await res.json();
+    if (status !== "ok") throw new Error("STATUS: NOT OK");
+    result.data = data;
+  } catch (error) {
+    result.error = error;
+  }
+  return result;
+};
+
+const retryWithRefreshToken = async (url, options = {}) => {
+  const refreshToken =
+    UserSessionManager.getInstance().getRefreshToken();
+
+  try {
+    const {
+      data: { access },
+      error,
+    } = await fetchData(Endpoints.REFRESH_TOKEN.href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refreshToken }),
+    });
+    if (error) throw new Error("Refresh: NOT OK");
+
+    UserSessionManager.getInstance().setAccessToken(access);
+  } catch (error) {
+    return { data: null, error };
+  }
+  return fetchAuthenticatedData(url, options, true);
+};
