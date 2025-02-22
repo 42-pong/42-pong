0aa115f7974d5c36253127e0ab0694e89716e4fb
habvi <93956895+habvi@users.noreply.github.com>
2025-02-23 00:58:43 +0900

    :zap:(users,me) viewで画像を受付けるようにparserを追加
    - JSONParser(display_name更新用): 通常のjson body
    - MultiPartParser(avatar更新用): 画像の場合のmedia_typeがmultipart/form-dataに対応

--- backend/pong/users/views/me.py
+++ backend/pong/users/views/me.py
@@ -10,6 +10,7 @@ from rest_framework import (
     status,
     views,
 )
+from rest_framework.parsers import JSONParser, MultiPartParser
 
 from accounts import constants as accounts_constants
 from pong.custom_response import custom_response
@@ -27,6 +28,8 @@ class UsersMeView(views.APIView):
 
     # プロフィールを全て返すのでIsAuthenticatedをセットする必要がある
     permission_classes = [permissions.IsAuthenticated]
+    # アバター画像用にMultiPartParserを追加
+    parser_classes = (JSONParser, MultiPartParser)
 
     def handle_exception(self, exc: Exception) -> response.Response:
         """
