7c085bbb690ea1abd3f56d1b1fff228c0f977c82
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-23 19:11:41 +0900

    :zap: (tournaments/tournament) トーナメントの状態を変更するときの制約を追加

--- backend/pong/tournaments/tournament/serializers.py
+++ backend/pong/tournaments/tournament/serializers.py
@@ -49,3 +49,47 @@ class TournamentCommandSerializer(drf_serializers.ModelSerializer):
                 "default": constants.TournamentFields.StatusEnum.NOT_STARTED.value,
             }
         }
+
+    def validate(self, data: dict) -> dict:
+        """
+        validate()のオーバーライド関数。
+
+        Raises:
+            ValidationError:
+                更新時:
+                    状態をON_GOINGにするときにまだ参加者がMAX_PARTICIPATIONS人いない場合。
+                    状態をCANCELEDにするときにまだ参加者がいる場合。
+        """
+        # 作成時のバリデーション
+        # return data
+        new_status = data.get(constants.TournamentFields.STATUS)
+
+        if self.instance:  # 更新時
+            # ON_GOING に変更する場合のバリデーション
+            if (
+                new_status
+                == constants.TournamentFields.StatusEnum.ON_GOING.value
+            ):
+                # MAX_PARTICIPATIONS 人に達していない場合はエラーを発生
+                max_participations = (
+                    constants.MAX_PARTICIPATIONS
+                )  # 定義されている最大参加人数
+                current_participants_count = (
+                    self.instance.tournament_participations.count()
+                )
+                if current_participants_count < max_participations:
+                    raise drf_serializers.ValidationError(
+                        f"Cannot set the status to ON_GOING with less than {max_participations} participants."
+                    )
+            # CANCELEDに変更する場合のバリデーション
+            if (
+                self.instance
+                and new_status
+                == constants.TournamentFields.StatusEnum.CANCELED.value
+            ):
+                if self.instance.tournament_participations.exists():
+                    raise drf_serializers.ValidationError(
+                        "Cannot cancel a tournament with participants."
+                    )
+
+        return data
