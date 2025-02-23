36f2f7933bb74a108af8662ffca5a5001a3284af
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-24 00:29:40 +0900

    :zap: (ws/routing) consumerがグローバルなTournamentManagerRegistryインスタンスにアクセスできるよう設定

--- backend/pong/ws/routing.py
+++ backend/pong/ws/routing.py
@@ -1,5 +1,17 @@
 from django.urls import path
 
+from ws.tournament import manager_registry
+
 from . import consumers
 
-websocket_urlpatterns = [path("ws/", consumers.MultiEventConsumer.as_asgi())]
+# グローバルな'TournamentManagerRegistry'を作成
+tournament_manager_registry = manager_registry.TournamentManagerRegistry()
+
+websocket_urlpatterns = [
+    path(
+        "ws/",
+        consumers.MultiEventConsumer.as_asgi(),
+        # consumerがself.scopeでTournamentManagerRegistryを受け取るために必要
+        {"tournament_manager_registry": tournament_manager_registry},
+    )
+]
