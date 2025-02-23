4f65882121b3174764cce80543cf3459fad07941
ayase-mstk <masa1215.hayase@gmail.com>
2025-02-23 19:15:42 +0900

    :zap: (tournaments/tournament) シリアライザ―のエラー時のテストを追加

--- backend/pong/tournaments/tournament/tests/test_command_serializers.py
+++ backend/pong/tournaments/tournament/tests/test_command_serializers.py
@@ -11,6 +11,11 @@ from ..serializers import TournamentCommandSerializer
 
 
 class TournamentCommandSerializerTest(TestCase):
+    def setUp(self):
+        # テスト用のトーナメントインスタンスを作成
+        self.tournament = models.Tournament.objects.create(
+            status=constants.TournamentFields.StatusEnum.NOT_STARTED.value
+        )
 
     def test_default_status(self):
         """
@@ -25,3 +30,47 @@ class TournamentCommandSerializerTest(TestCase):
             serializer.validated_data[constants.TournamentFields.STATUS],
             constants.TournamentFields.StatusEnum.NOT_STARTED.value,
         )
+
+    def test_on_going_status_with_insufficient_participants(self):
+        """
+        `status` を `ON_GOING` に変更する際、参加者が足りない場合にエラーが発生するかを確認するテスト
+        """
+        serializer_data = {
+            constants.TournamentFields.STATUS: constants.TournamentFields.StatusEnum.ON_GOING.value,
+        }
+        serializer = TournamentCommandSerializer(
+            self.tournament, data=serializer_data
+        )
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
+
+    def test_cancelled_status_with_participants(self):
+        """
+        参加者がいる場合に `status` を `CANCELED` に変更できないことを確認するテスト
+        """
+        # プレーヤーのモックを作成
+        user = User.objects.create(
+            username="testuser_1",
+            email="testuser_1@example.com",
+            password="testpassword",
+        )
+        player = player_models.Player.objects.create(
+            user=user, display_name="Test Player"
+        )
+
+
+        # 参加者を追加
+        participation_models.Participation.objects.create(
+            tournament=self.tournament,
+            player=player,
+            participation_name="Player 1",
+        )
+
+        serializer_data = {
+            constants.TournamentFields.STATUS: constants.TournamentFields.StatusEnum.CANCELED.value,
+        }
+        serializer = TournamentCommandSerializer(
+            self.tournament, data=serializer_data
+        )
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
