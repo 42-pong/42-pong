8d1f376f6fa346059abc878bdd10b57e4b277a32
taekklee <taekklee@student.42tokyo.jp>
2025-02-24 20:07:33 +0900

    :bug: (FE:setBorderWithShadow) 変数の指定ミスを修正

--- frontend/pong/js/utils/setBorderWithShadow.js
+++ frontend/pong/js/utils/setBorderWithShadow.js
@@ -2,7 +2,7 @@ import { BootstrapBorders } from "../bootstrap/utilities/borders";
 import { BootstrapShadows } from "../bootstrap/utilities/shadows";
 
 export const setBorderWithShadow = (element) => {
-  BootstrapBorders.setBorder(this);
-  BootstrapBorders.setRounded(this);
-  BootstrapShadows.setShadowLarge(this);
+  BootstrapBorders.setBorder(element);
+  BootstrapBorders.setRounded(element);
+  BootstrapShadows.setShadow(element);
 };
