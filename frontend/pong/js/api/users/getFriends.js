bb128da23b1ba1827dfdf388dcb0f419776759e5
taekklee <taekklee@student.42tokyo.jp>
2025-02-27 05:02:15 +0900

    :fire: (FE:getFriends) 対応済みの認証つきへの移行 TODO を削除

--- frontend/pong/js/api/users/getFriends.js
+++ frontend/pong/js/api/users/getFriends.js
@@ -3,7 +3,6 @@ import { fetchAuthenticatedData } from "../utils/fetchAuthenticatedData";
 import { convertFriendData } from "./convertFriendData";
 
 export async function getFriends() {
-  // TODO: 認証つきのものに変更
   const { data, error } = await fetchAuthenticatedData(
     Endpoints.FRIENDS.href,
   );
