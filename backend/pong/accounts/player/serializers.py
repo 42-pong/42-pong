f0b2d1573374a8520fb276c253d873630b50bce0
habvi <93956895+habvi@users.noreply.github.com>
2025-02-26 22:22:39 +0900

    :zap:(avatar) アバター画像の拡張子を制限するvalidator追加

--- backend/pong/accounts/player/serializers.py
+++ backend/pong/accounts/player/serializers.py
@@ -1,9 +1,30 @@
+import os
+
 from django.contrib.auth.models import User
+from django.core.files import File
 from django.core.validators import RegexValidator
 from rest_framework import serializers
 
 from .. import constants
 from . import models
+
+
+def validate_avatar_extension(avatar: File) -> None:
+    """
+    avatarの拡張子に制限をかけるバリデータ
+    extensionが空文字列の場合はImageFeildのバリデータで先にエラーになるためここではチェックしない
+    """
+    valid_extensions: set[str] = {".png", ".jpg", ".jpeg", ".gif"}
+    # mypyにavatar.nameがNoneの可能性を指摘されるが、ImageFieldのvalidatorでextensionがあることは確認済みのため無視
+    _, extension = os.path.splitext(avatar.name)  # type: ignore[type-var]
+    # is Noneも確認しないとmypyのエラーになるので念のため追加
+    if extension is None:
+        raise serializers.ValidationError("Must contain a file extension.")
+    if extension.lower() not in valid_extensions:
+        raise serializers.ValidationError(
+            f"Unsupported file extension: {extension}. "
+            f"Supported extensions are {", ".join(valid_extensions)}."
+        )
 
 
 class PlayerSerializer(serializers.ModelSerializer):
@@ -33,8 +54,9 @@ class PlayerSerializer(serializers.ModelSerializer):
     avatar: serializers.ImageField = serializers.ImageField(
         required=False,
         allow_null=True,
-        # todo: 画像の最大サイズを指定・拡張子を制限・リサイズする
+        # todo: 画像の最大サイズを指定・リサイズする
         # max_length=constants.MAX_AVATAR_SIZE,
+        validators=[validate_avatar_extension],
     )
 
     class Meta:
