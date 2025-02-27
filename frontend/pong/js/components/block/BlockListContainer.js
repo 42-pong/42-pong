40e57a54b4812bd9a2357ac03c8a48b01a0cc7a2
taekklee <taekklee@student.42tokyo.jp>
2025-02-28 03:10:46 +0900

    :zap: (FE:BlockListContainer) ブロック 0 人のハンドリング追加

--- frontend/pong/js/components/block/BlockListContainer.js
+++ frontend/pong/js/components/block/BlockListContainer.js
@@ -1,8 +1,12 @@
 import { getBlocks } from "../../api/users/getBlocks";
 import { BootstrapBadge } from "../../bootstrap/components/badge";
+import { BootstrapBorders } from "../../bootstrap/utilities/borders";
+import { BootstrapDisplay } from "../../bootstrap/utilities/display";
+import { BootstrapFlex } from "../../bootstrap/utilities/flex";
 import { BootstrapSizing } from "../../bootstrap/utilities/sizing";
 import { BootstrapSpacing } from "../../bootstrap/utilities/spacing";
 import { Component } from "../../core/Component";
+import { createDefaultFlexBox } from "../../utils/elements/div/createFlexBox";
 import { createHorizontalSplitLayout } from "../../utils/elements/div/createHorizontalSplitLayout";
 import { createTextElement } from "../../utils/elements/span/createTextElement";
 import { setHeight } from "../../utils/elements/style/setHeight";
@@ -13,29 +17,22 @@ export class BlockListContainer extends Component {
   #blockList;
 
   constructor(state = {}) {
-    super(state);
-    this.#blockList = null;
+    super({ items: [], ...state });
   }
 
   _setStyle() {
-    setHeight(this.#blockList, "50vh");
-    BootstrapSizing.setWidth100(this.#blockList);
-    BootstrapSpacing.setMargin(this.#blockList);
+    BootstrapDisplay.setFlex(this);
+    BootstrapFlex.setAlignItemsCenter(this);
+    BootstrapFlex.setJustifyContentCenter(this);
   }
 
   _onConnect() {
-    this.#blockList = new ListContainer({
-      ListItem: BlockListItem,
-      items: [],
-    });
-
     getBlocks().then(({ users, error }) => {
       if (error) {
         this._updateState({ isError: true });
         return;
       }
-      if (this.#blockList)
-        this.#blockList._updateState({ items: users });
+      this._updateState({ items: users });
     });
   }
 
@@ -57,12 +54,39 @@ export class BlockListContainer extends Component {
       5,
       BootstrapBadge.setDanger,
     );
-    const layout = createHorizontalSplitLayout(
-      title,
-      this.#blockList,
-    );
+
+    const { items } = this._getState();
+    const blockPanel =
+      items.length > 0 ? createBlockList(items) : noBlockedUser();
+
+    const layout = createHorizontalSplitLayout(title, blockPanel);
+    BootstrapSpacing.setPadding(layout, 3);
+    BootstrapSizing.setWidth100(layout);
     BootstrapSizing.setHeight100(layout);
     BootstrapSpacing.setMargin(title);
     this.append(layout);
   }
 }
+
+const noBlockedUser = () => {
+  const content = createTextElement(
+    "ユーザーが見つかりませでした",
+    5,
+    BootstrapBadge.setSecondary,
+  );
+  const wrapper = createDefaultFlexBox(content);
+  setHeight(wrapper, "50vh");
+  BootstrapSpacing.setPadding(wrapper, 3);
+  return wrapper;
+};
+
+const createBlockList = (items) => {
+  const blockList = new ListContainer({
+    ListItem: BlockListItem,
+    items,
+  });
+  setHeight(blockList, "50vh");
+  BootstrapSizing.setWidth100(blockList);
+  BootstrapSpacing.setMargin(blockList);
+  return blockList;
+};
