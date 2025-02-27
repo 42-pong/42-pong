3d998c4cd87a43b084913bc6b786e6472b560a09
s1-haya <sunzecun@gmail.com>
2025-02-27 09:48:40 +0900

    :zap: (BE:jwt) CustomJWTAuthenticationSchemeのdocstringの追加

--- backend/pong/jwt/schema.py
+++ backend/pong/jwt/schema.py
@@ -4,10 +4,19 @@ from drf_spectacular.plumbing import build_bearer_security_scheme_object
 
 
 class CustomJWTAuthenticationScheme(OpenApiAuthenticationExtension):  # type: ignore
+    """
+    OpenAPI 用のカスタム JWT 認証スキーム拡張クラス
+    """
+
     target_class = "jwt.authentication.CustomJWTAuthentication"
     name = "jwtAuth"
 
     def get_security_definition(self, auto_schema: AutoSchema) -> dict:
+        """
+        OpenApiAuthenticationExtensionが提供するOpenAPI の認証方式を定義する関数
+
+        本メソッドをオーバーライドし、JWT の Bearer 認証方式を使用するように設定する
+        """
         return build_bearer_security_scheme_object(
             header_name="Authorization",
             token_prefix="Bearer",
