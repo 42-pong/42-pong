a62edfe2e536377b54136d0e0e0231ba28837dd5
ayase-mstk <masa1215.hayase@gmail.com>
2025-03-01 00:58:07 +0900

    :zap: (ws/tournament) ラウンドの状態更新関数の単体テストをpytestで追加

--- backend/pong/ws/tournament/tests/pytest_async_db_service.py
+++ backend/pong/ws/tournament/tests/pytest_async_db_service.py
@@ -7,6 +7,7 @@ from tournaments.round.models import Round
 from tournaments.tournament.models import Tournament
 from ws.tournament.async_db_service import (
     create_round,
+    update_round_status,
 )
 
 
@@ -41,3 +42,30 @@ async def test_create_round(create_tournament: Tournament) -> None:
         result_value[RoundFields.STATUS]
         == RoundFields.StatusEnum.NOT_STARTED.value
     )
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_update_round_status(
+    create_tournament: Tournament,
+) -> None:
+    tournament = create_tournament
+
+    # 試合を作成
+    round = await database_sync_to_async(Round.objects.create)(
+        tournament_id=tournament.id, round_number=1
+    )
+
+    # 参加者を作成
+    new_status = RoundFields.StatusEnum.COMPLETED.value
+
+    # update_participation_is_winを呼び出し、勝利ステータスを更新
+    result = await update_round_status(round.id, new_status)
+
+    # 結果の検証
+    assert result.is_ok
+    result_value = result.unwrap()
+    assert result_value[RoundFields.ID] is not None
+    assert result_value[RoundFields.TOURNAMENT_ID] == tournament.id
+    assert result_value[RoundFields.ROUND_NUMBER] == 1
+    assert result_value[RoundFields.STATUS] == new_status
