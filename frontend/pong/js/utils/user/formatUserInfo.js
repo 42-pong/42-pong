b7f89ab07feb6b50c2bbfe556b2902cab65c8e63
taekklee <taekklee@student.42tokyo.jp>
2025-03-02 00:44:03 +0900

    :zap: (FE:friend) フレンド表示時にオンラインステータス追加

--- frontend/pong/js/utils/user/formatUserInfo.js
+++ frontend/pong/js/utils/user/formatUserInfo.js
@@ -6,9 +6,13 @@ export const formatUserInfo = (user) =>
         nameTagText: `${user.displayName}#${user.username}`,
         avatarPathname: user.avatar,
         avatarAlt: `${user.displayName}#${user.username}'s avatar`,
+        isDisplayingStatus: user.isDisplayingStatus ?? false,
+        id: user.id,
       }
     : {
         nameTagText: "...",
         avatarPathname: Endpoints.USERS.defaultAvatar.href,
         avatarAlt: "placeholder",
+        isDisplayingStatus: false,
+        id: null,
       };
