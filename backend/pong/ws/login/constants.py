cb4ff105d695a56c01821ada55daa07ab5b5433a
ayase-mstk <masa1215.hayase@gmail.com>
2025-03-02 12:05:01 +0900

    :zap: (ws/login) ログインメッセージのルール変更に伴い、定数を追加

--- backend/pong/ws/login/constants.py
+++ backend/pong/ws/login/constants.py
@@ -6,6 +6,8 @@ USER_NAMESPACE: Final[str] = "user"
 CHANNEL_RESOURCE: Final[str] = "channels"
 
 USER_ID: Final[str] = "user_id"
+ONLINE_FRIEND_IDS: Final[str] = "online_friend_ids"
+ONLINE: Final[str] = "online"
 
 
 class Status(ws_constants.BaseEnum):
