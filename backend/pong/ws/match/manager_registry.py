4d3044a63f7791bc97fa77bd36199399f438e4d2
ayase-mstk <masa1215.hayase@gmail.com>
2025-03-02 14:39:09 +0900

    :zap: (ws/match) MatchManagerをConsumerから操作するためのクラスを作成
    - TournamentManagerまたはMatchHandlerで作成し、そのMatchManagerをこのクラスに登録させる想定です。

---
+++ backend/pong/ws/match/manager_registry.py
@@ -0,0 +1,52 @@
+import asyncio
+from typing import Optional
+
+from ws.share import player_data
+
+from . import match_manager
+
+
+class MatchManagerRegistry:
+    """
+    MatchManagerにConsumerからアクセスするための関数。
+
+    リモート対戦の場合はTournamentManagerが生成し、MatchManagerを登録する。
+    ローカル対戦の場合はMatchHandlerが作成し、MatchManagerを登録する。
+
+    試合が始まったら、Consumerの入力をこのクラスを通してMatchManagerに渡す。
+    """
+
+    def __init__(self) -> None:
+        self.managers: dict[
+            int, match_manager.MatchManager
+        ] = {}  # { tournament_id: TournamentManager }
+        self.lock = asyncio.Lock()
+
+    async def register_match_manager(
+        self, match_id: int, manager: match_manager.MatchManager
+    ) -> None:
+        """
+        渡されたmatch_idでMatchManagerを登録
+        すでに登録済みなら無視する。
+        """
+        async with self.lock:
+            if match_id not in self.managers:
+                self.managers[match_id] = manager
+
+    async def delete_match(self, match_id: int) -> None:
+        """
+        渡されたmatch_idのMatchManagerがあれば削除する
+        存在しない場合は何もしない
+        """
+        async with self.lock:
+            if match_id in self.managers:
+                del self.managers[match_id]
+
+    async def get_match(
+        self, match_id: int
+    ) -> Optional[match_manager.MatchManager]:
+        """
+        渡されたmatch_idのMatchManagerがあれば返す。
+        なければNoneを返す
+        """
+        return self.managers.get(match_id, None)
