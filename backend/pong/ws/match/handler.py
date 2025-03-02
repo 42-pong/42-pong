6e434a576b7ac200c4001db801df380582dabc63
ayase-mstk <masa1215.hayase@gmail.com>
2025-03-03 01:19:03 +0900

    :zap: (ws/match) MatchHandler のローカルプレイの部分を実装

--- backend/pong/ws/match/handler.py
+++ backend/pong/ws/match/handler.py
@@ -2,7 +2,9 @@ import asyncio
 from typing import Optional
 
 from ..share import constants as ws_constants
+from ..share import player_data
 from . import constants as match_constants
+from . import match_manager
 from . import serializers as match_serializers
 
 
@@ -25,7 +27,13 @@ class MatchHandler:
             match_constants.Stage.PLAY.value: self._handle_play,
             match_constants.Stage.END.value: self._handle_end,
         }
-        self.is_local: bool = True
+        self.is_local_play: bool = True
+        self.match_manager: Optional[match_manager.MatchManager] = None
+        self.player_data: player_data.PlayerData = player_data.PlayerData(
+            channel_name=channel_name,
+            user_id=None,
+            participation_name=None,
+        )
 
     def __str__(self) -> str:
         """
@@ -33,7 +41,10 @@ class MatchHandler:
 
         :return: MatchHandlerオブジェクトの文字列表現
         """
-        return f"MatchHandler(stage={self.stage})"
+        return (
+            f"MatchHandler(stage={self.stage}, "
+            f"player_data={self.player_data})"
+        )
 
     def __repr__(self) -> str:
         """
@@ -43,19 +54,28 @@ class MatchHandler:
         """
         return (
             f"MatchHandler(stage={self.stage}, "
-            f"local_play={self.local_play}, "
-            f"channel_handler={self.channel_handler!r})"
+            f"is_local_play={self.is_local_play}, "
+            f"match_manager={self.match_manager}, "
+            f"player_data={self.player_data})"
         )
 
     # ===================
     # ハンドラーメソッド
     # ===================
-    async def handle(self, payload: dict) -> None:
+    async def handle(self, payload: dict, user_id: Optional[int]) -> None:
         """
         プレイヤーからの入力を受け取り、ステージごとに処理を振り分ける。
 
         :param payload: プレイヤーから送られてきたペイロード
         """
+        # ログイン後の初回にプレーヤーデータを作成
+        if self.player_data.user_id is None and user_id is not None:
+            self.player_data = player_data.PlayerData(
+                channel_name=self.player_data.channel_name,
+                user_id=user_id,
+                participation_name=None,
+            )
+
         serializer = match_serializers.MatchInputSerializer(data=payload)
         serializer.is_valid(raise_exception=True)
         data: dict = serializer.validated_data[ws_constants.DATA_KEY]
@@ -77,17 +97,20 @@ class MatchHandler:
         :param data: 初期化に必要なデータ
         """
         self.stage = match_constants.Stage.INIT
+        match_id: int = data[match_constants.MATCH_ID]
+        mode: str = data[match_constants.Mode.key()]
         # プレイモードによって所属させるグループを変える
-        if (
-            data[match_constants.Mode.key()]
-            == match_constants.Mode.LOCAL.value
-        ):
-            TODO: マッチマネジャーを作成して、ローカルゲームのセットアップを行う。
-        elif (
-            data[match_constants.Mode.key()]
-            == match_constants.Mode.REMOTE.value
-        ):
-            self.is_local = False
+        if mode == match_constants.Mode.LOCAL.value:
+            # TODO: マッチマネジャーを作成して、ローカルゲームのセットアップを行う。
+            self.match_manager = match_manager.MatchManager(
+                match_id, self.player_data, None, mode
+            )
+            self.local_match_task = asyncio.create_task(
+                self.match_manager.run()
+            )
+            await self.match_manager.handle_init_action(self.player_data)
+        elif mode == match_constants.Mode.REMOTE.value:
+            self.is_local_play = False
 
         # TODO: remoteの場合のグループ作成方法は別で考える
 
@@ -101,6 +124,10 @@ class MatchHandler:
         :param data: 準備状態に必要なデータ
         """
         self.stage = match_constants.Stage.READY
+        if self.is_local_play and self.match_manager is not None:
+            await self.match_manager.handle_ready_action(self.player_data)
+        else:
+            pass
 
         # ゲーム状況の更新をしてプレーヤーに非同期で送信し続ける処理を開始する
         self.stage = match_constants.Stage.PLAY
@@ -112,21 +139,34 @@ class MatchHandler:
 
         :param data: パドルの移動情報
         """
-        self._move_paddle(paddle_move=data)
+        team: str = data[match_constants.Team.key()]
+        move: str = data[match_constants.Move.key()]
+
+        if self.is_local_play and self.match_manager is not None:
+            if move == match_constants.Move.UP.value:
+                await self.match_manager.paddle_up(team)
+            elif move == match_constants.Move.DOWN.value:
+                await self.match_manager.paddle_down(team)
+        else:
+            pass
 
     async def _handle_end(self, data: dict) -> None:
         """
         ENDステージのメッセージが送られてきたときの処理
         プレーヤーがmatchを退出したときの処理を行う。
         """
-        await self.cleanup()
+        if (
+            self.is_local_play
+            and self.local_match_task
+            and not self.local_match_task.done()
+        ):
+            self.local_match_task.cancel()  # タスクをキャンセル
+            try:
+                # キャンセル後、タスクが終了するのを待つ
+                await self.local_match_task
+            except asyncio.CancelledError:
+                pass
 
-    async def _end_process(self) -> None:
-        """
-        ゲーム終了時の処理。
-
-        ENDステージのメッセージを送信し、クリーンナップ処理を行う。
-        """
         await self.cleanup()
 
     async def cleanup(self) -> None:
@@ -135,4 +175,5 @@ class MatchHandler:
         グループから削除し、状態を初期化する
         """
         self.stage = None
-        self.is_local = True
+        self.is_local_play = True
+        self.match_manager = None
