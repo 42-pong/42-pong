0369a1f693d3d19429ec5d0b4a91536e8bbfbf2b
taekklee <taekklee@student.42tokyo.jp>
2025-03-07 00:48:11 +0900

    :seedling: (FE:oauth) 認証終わりで BE からのデータと連携

--- frontend/pong/js/main.js
+++ frontend/pong/js/main.js
@@ -5,13 +5,15 @@ import { PongEvents } from "./constants/PongEvents";
 import { Route } from "./core/Route";
 import { appRouter } from "./routers/appRouter";
 import { UserSessionManager } from "./session/UserSessionManager";
+import { handleAuthPath } from "./utils/handleAuthPath";
 
 function main() {
   const app = document.getElementById("app");
   const appLocal = document.getElementById("app-local");
   const appGlobal = document.getElementById("app-global");
   const router = appRouter(appLocal);
-  const updateWindowPath = () => {
+  const updateWindowPath = async () => {
+    if (await handleAuthPath()) return;
     router.update(window.location.pathname);
   };
   window.addEventListener("popstate", updateWindowPath);
