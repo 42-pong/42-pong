59cb02ad72070c0db048afb867ced01e5365a3d9
taekklee <taekklee@student.42tokyo.jp>
2025-03-08 14:10:20 +0900

    :seedling: (FE:2fa) ログインからの 2fa ための画面を用意

--- frontend/pong/js/components/views/LoginView.js
+++ frontend/pong/js/components/views/LoginView.js
@@ -2,14 +2,18 @@ import { BootstrapDisplay } from "../../bootstrap/utilities/display";
 import { BootstrapFlex } from "../../bootstrap/utilities/flex";
 import { BootstrapSizing } from "../../bootstrap/utilities/sizing";
 import { BootstrapSpacing } from "../../bootstrap/utilities/spacing";
+import { PongEvents } from "../../constants/PongEvents";
 import { AuthView } from "../../core/AuthView";
 import { LoginContainer } from "../auth/LoginContainer";
+import { Verify2faContainer } from "../auth/Verify2faContainer";
 
 export class LoginView extends AuthView {
-  #main;
-
   constructor(state = {}) {
-    super({ authType: AuthView.Type.PROHIBITED, ...state });
+    super({
+      authType: AuthView.Type.PROHIBITED,
+      is2faRunning: false,
+      ...state,
+    });
   }
 
   _setStyle() {
@@ -19,16 +23,28 @@ export class LoginView extends AuthView {
     BootstrapFlex.setAlignItemsCenter(this);
     BootstrapSizing.setViewportHeight100(this);
     BootstrapSpacing.setPadding(this);
-
-    BootstrapSizing.setWidth50(this.#main);
-    BootstrapSizing.setHeight50(this.#main);
   }
 
   _onConnect() {
-    this.#main = new LoginContainer();
+    this._attachEventListener(PongEvents.VERIFY_2FA.type, (event) => {
+      event.preventDefault();
+      const {
+        detail: { credentials },
+      } = event;
+      this._updateState({
+        is2faRunning: true,
+        credentials,
+      });
+    });
   }
 
   _render() {
-    this.appendChild(this.#main);
+    const { is2faRunning, credentials } = this._getState();
+    const main = is2faRunning
+      ? new Verify2faContainer({ credentials })
+      : new LoginContainer();
+    BootstrapSizing.setWidth50(main);
+    BootstrapSizing.setHeight50(main);
+    this.append(main);
   }
 }
